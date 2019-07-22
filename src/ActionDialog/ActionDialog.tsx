b88599223e8fb7e2532fdf31b81aa58b18e85a21
dominik-zeglen <flesz3@o2.pl>
2019-07-22 19:38:37 +0200

    Initial commit

---
+++ src/ActionDialog/ActionDialog.tsx
@@ -0,0 +1,80 @@
+import Button from "@material-ui/core/Button";
+import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import { Theme } from "@material-ui/core/styles";
+import makeStyles from "@material-ui/styles/makeStyles";
+import classNames from "classnames";
+import React from "react";
+
+import ConfirmButton, {
+  ConfirmButtonTransitionState
+} from "../ConfirmButton/ConfirmButton";
+
+const useStyles = makeStyles((theme: Theme) => ({
+  danger: {
+    "&:hover": {
+      backgroundColor: theme.palette.error.main
+    },
+    backgroundColor: theme.palette.error.main,
+    color: theme.palette.error.contrastText
+  }
+}));
+
+export interface ActionDialogProps {
+  children?: React.ReactNode;
+  cancelLabel: React.ReactNode;
+  confirmLabel: React.ReactNode;
+  confirmButtonState: ConfirmButtonTransitionState;
+  maxWidth?: "xs" | "sm" | "md" | "lg" | "xl" | false;
+  open: boolean;
+  title: React.ReactNode;
+  variant?: "default" | "danger";
+  onClose: () => void;
+  onConfirm: () => void;
+}
+
+const ActionDialog: React.FC<ActionDialogProps> = props => {
+  const {
+    children,
+    cancelLabel,
+    confirmLabel,
+    confirmButtonState,
+    open,
+    maxWidth,
+    title,
+    variant,
+    onConfirm,
+    onClose
+  } = props;
+  const classes = useStyles(props);
+
+  return (
+    <Dialog onClose={onClose} open={open} fullWidth maxWidth={maxWidth}>
+      <DialogTitle>{title}</DialogTitle>
+      <DialogContent>{children}</DialogContent>
+      <DialogActions>
+        <Button onClick={onClose}>{cancelLabel}</Button>
+        <ConfirmButton
+          transitionState={confirmButtonState}
+          color="primary"
+          variant="contained"
+          onClick={onConfirm}
+          className={classNames({
+            [classes.danger]: variant === "danger"
+          })}
+        >
+          {confirmLabel}
+        </ConfirmButton>
+      </DialogActions>
+    </Dialog>
+  );
+};
+
+ActionDialog.defaultProps = {
+  maxWidth: "xs",
+  variant: "default"
+};
+ActionDialog.displayName = "ActionDialog";
+export default ActionDialog;
---
+++ src/ConfirmButton/ConfirmButton.tsx
@@ -0,0 +1,196 @@
+import { Omit } from "@material-ui/core";
+import Button, { ButtonProps } from "@material-ui/core/Button";
+import CircularProgress from "@material-ui/core/CircularProgress";
+import {
+  createStyles,
+  Theme,
+  withStyles,
+  WithStyles
+} from "@material-ui/core/styles";
+import CheckIcon from "@material-ui/icons/Check";
+import CloseIcon from "@material-ui/icons/Close";
+import classNames from "classnames";
+import React from "react";
+
+export type ConfirmButtonTransitionState =
+  | "loading"
+  | "success"
+  | "error"
+  | "default";
+
+const styles = (theme: Theme) =>
+  createStyles({
+    error: {
+      "&:hover": {
+        backgroundColor: theme.palette.error.main
+      },
+      backgroundColor: theme.palette.error.main,
+      color: theme.palette.error.contrastText
+    },
+    icon: {
+      marginLeft: "0 !important",
+      position: "absolute",
+      transitionDuration: theme.transitions.duration.standard + "ms"
+    },
+    invisible: {
+      opacity: 0
+    },
+    label: {
+      alignItems: "center",
+      display: "flex",
+      transitionDuration: theme.transitions.duration.standard + "ms"
+    },
+    progress: {
+      "& svg": {
+        color: theme.palette.common.white,
+        margin: 0
+      },
+      position: "absolute",
+      transitionDuration: theme.transitions.duration.standard + "ms"
+    },
+    success: {
+      "&:hover": {
+        backgroundColor: theme.palette.primary.main
+      },
+      backgroundColor: theme.palette.primary.main,
+      color: theme.palette.primary.contrastText
+    }
+  });
+
+export interface ConfirmButtonProps extends Omit<ButtonProps, "classes"> {
+  errorLabel?: React.ReactNode;
+  successLabel?: React.ReactNode;
+  transitionState: ConfirmButtonTransitionState;
+}
+
+interface ConfirmButtonState {
+  displayCompletedActionState: boolean;
+}
+
+const ConfirmButton = withStyles(styles)(
+  class ConfirmButtonComponent extends React.Component<
+    ConfirmButtonProps &
+      WithStyles<
+        "error" | "icon" | "invisible" | "label" | "progress" | "success"
+      >,
+    ConfirmButtonState
+  > {
+    static getDerivedStateFromProps(
+      nextProps: ConfirmButtonProps,
+      prevState: ConfirmButtonState
+    ): ConfirmButtonState {
+      if (nextProps.transitionState === "loading") {
+        return {
+          displayCompletedActionState: true
+        };
+      }
+      return prevState;
+    }
+
+    state: ConfirmButtonState = {
+      displayCompletedActionState: false
+    };
+    timeout = null;
+
+    componentDidUpdate(prevProps: ConfirmButtonProps) {
+      const { transitionState } = this.props;
+      if (prevProps.transitionState !== transitionState) {
+        if (
+          (["error", "success"] as ConfirmButtonTransitionState[]).includes(
+            transitionState
+          )
+        ) {
+          this.timeout = setTimeout(
+            () =>
+              this.setState({
+                displayCompletedActionState: false
+              }),
+            2000
+          );
+        } else if (transitionState === "loading") {
+          clearTimeout(this.timeout);
+        }
+      }
+    }
+
+    componentWillUnmount() {
+      clearTimeout(this.timeout);
+    }
+
+    render() {
+      const {
+        children,
+        classes,
+        className,
+        disabled,
+        errorLabel,
+        successLabel,
+        transitionState,
+        onClick,
+        ...props
+      } = this.props;
+      const { displayCompletedActionState } = this.state;
+
+      return (
+        <Button
+          variant="contained"
+          onClick={transitionState === "loading" ? undefined : onClick}
+          color="primary"
+          className={classNames({
+            [classes.error]:
+              transitionState === "error" && displayCompletedActionState,
+            [classes.success]:
+              transitionState === "success" && displayCompletedActionState,
+            [className]: true
+          })}
+          disabled={!displayCompletedActionState && disabled}
+          {...props}
+        >
+          <CircularProgress
+            size={24}
+            color="inherit"
+            className={classNames({
+              [classes.progress]: true,
+              [classes.invisible]: transitionState !== "loading"
+            })}
+          />
+          {successLabel ? (
+            <span
+              className={classNames({
+                [classes.invisible]: !(
+                  transitionState === "success" && displayCompletedActionState
+                )
+              })}
+            >
+              {successLabel}
+            </span>
+          ) : (
+            <CheckIcon
+              className={classNames({
+                [classes.icon]: true,
+                [classes.invisible]: !(
+                  transitionState === "success" && displayCompletedActionState
+                )
+              })}
+            />
+          )}
+          <span
+            className={classNames({
+              [classes.label]: true,
+              [classes.invisible]:
+                (transitionState === "loading" ||
+                  transitionState === "success") &&
+                displayCompletedActionState
+            })}
+          >
+            {transitionState === "error" && displayCompletedActionState
+              ? errorLabel || <CloseIcon className={classes.icon} />
+              : children}
+          </span>
+        </Button>
+      );
+    }
+  }
+);
+ConfirmButton.displayName = "ConfirmButton";
+export default ConfirmButton;
