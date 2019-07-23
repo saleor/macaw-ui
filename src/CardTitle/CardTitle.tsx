65f2274acf784f7d782c37cc41f09016a32a5736
dominik-zeglen <flesz3@o2.pl>
2019-07-23 17:27:55 +0200

    Add card title

---
+++ src/CardTitle/CardTitle.tsx
@@ -0,0 +1,80 @@
+import { Theme } from "@material-ui/core/styles";
+import Typography from "@material-ui/core/Typography";
+import makeStyles from "@material-ui/styles/makeStyles";
+import classNames from "classnames";
+import React from "react";
+
+const useStyles = makeStyles((theme: Theme) => ({
+  children: theme.mixins.gutters({}),
+  constantHeight: {
+    height: 56
+  },
+  hr: {
+    border: "none",
+    borderTop: `1px solid ${theme.overrides.MuiCard.root.borderColor}`,
+    height: 0,
+    marginBottom: 0,
+    marginTop: 0,
+    width: "100%"
+  },
+  root: theme.mixins.gutters({
+    alignItems: "center",
+    display: "flex",
+    minHeight: 56
+  }),
+  title: {
+    flex: 1,
+    lineHeight: 1
+  },
+  toolbar: {
+    marginRight: -theme.spacing.unit
+  }
+}));
+
+interface CardTitleProps {
+  children?: React.ReactNode;
+  className?: string;
+  height?: "default" | "const";
+  title: string | React.ReactNode;
+  toolbar?: React.ReactNode;
+  onClick?: (event: React.MouseEvent<any>) => void;
+}
+
+const CardTitle: React.FC<CardTitleProps> = props => {
+  const {
+    className,
+    children,
+    height,
+    title,
+    toolbar,
+    onClick,
+    ...rest
+  } = props;
+  const classes = useStyles(props);
+
+  return (
+    <>
+      <div
+        className={classNames(classes.root, {
+          [className]: !!className,
+          [classes.constantHeight]: height === "const"
+        })}
+        {...rest}
+      >
+        <Typography
+          className={classes.title}
+          variant="h5"
+          onClick={onClick}
+          component="span"
+        >
+          {title}
+        </Typography>
+        <div className={classes.toolbar}>{toolbar}</div>
+      </div>
+      <div className={classes.children}>{children}</div>
+      <hr className={classes.hr} />
+    </>
+  );
+};
+CardTitle.displayName = "CardTitle";
+export default CardTitle;
