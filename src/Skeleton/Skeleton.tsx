c3f10fec74fe43dd14dc3ced4be6ca87b9fd8cca
dominik-zeglen <flesz3@o2.pl>
2020-02-04 15:26:09 +0100

    Add missing name to makestyles hook

--- src/Skeleton/Skeleton.tsx
+++ src/Skeleton/Skeleton.tsx
@@ -1,31 +1,33 @@
-import { Theme } from "@material-ui/core/styles";
-import makeStyles from "@material-ui/styles/makeStyles";
+import { makeStyles } from "@material-ui/core/styles";
 import classNames from "classnames";
 import React from "react";
 
-const useStyles = makeStyles((theme: Theme) => ({
-  "@keyframes skeleton-animation": {
-    "0%": {
-      opacity: 0.6
+const useStyles = makeStyles(
+  theme => ({
+    "@keyframes skeleton-animation": {
+      "0%": {
+        opacity: 0.6
+      },
+      "100%": {
+        opacity: 1
+      }
     },
-    "100%": {
-      opacity: 1
+    primary: {
+      "&$skeleton": {
+        background: theme.palette.primary.main
+      }
+    },
+    skeleton: {
+      animation: "skeleton-animation .75s linear infinite forwards alternate",
+      background: theme.palette.background.default,
+      borderRadius: 4,
+      display: "block",
+      height: "0.8em",
+      margin: "0.2em 0"
     }
-  },
-  primary: {
-    "&$skeleton": {
-      background: theme.palette.primary.main
-    }
-  },
-  skeleton: {
-    animation: "skeleton-animation .75s linear infinite forwards alternate",
-    background: theme.palette.background.default,
-    borderRadius: 4,
-    display: "block",
-    height: "0.8em",
-    margin: "0.2em 0"
-  }
-}));
+  }),
+  { name: "Skeleton" }
+);
 
 interface SkeletonProps {
   className?: string;
@@ -35,7 +37,9 @@ interface SkeletonProps {
 
 const Skeleton: React.FC<SkeletonProps> = props => {
   const { className, primary, ...rest } = props;
+
   const classes = useStyles(props);
+
   return (
     <span
       className={classNames(classes.skeleton, className, {
