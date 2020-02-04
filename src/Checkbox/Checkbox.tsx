68cf16fc65fec7b3fcdf2b9e8afbb24473c5eae9
dominik-zeglen <flesz3@o2.pl>
2020-02-04 16:53:08 +0100

    Update checkbox component

--- src/Checkbox/Checkbox.tsx
+++ src/Checkbox/Checkbox.tsx
@@ -1,11 +1,9 @@
 import ButtonBase from "@material-ui/core/ButtonBase";
 import { CheckboxProps as MuiCheckboxProps } from "@material-ui/core/Checkbox";
-import { Theme } from "@material-ui/core/styles";
-import makeStyles from "@material-ui/styles/makeStyles";
+import { makeStyles } from "@material-ui/core/styles";
+import { fade } from "@material-ui/core/styles/colorManipulator";
 import classNames from "classnames";
 import React from "react";
-
-import { stopPropagation } from "../utils/events";
 
 export type CheckboxProps = Omit<
   MuiCheckboxProps,
@@ -15,76 +13,101 @@ export type CheckboxProps = Omit<
   | "indeterminateIcon"
   | "classes"
   | "onChange"
+  | "onClick"
 > & {
+  disableClickPropagation?: boolean;
   onChange?: (event: React.ChangeEvent<any>) => void;
 };
 
-const useStyles = makeStyles((theme: Theme) => ({
-  box: {
-    "&$checked": {
-      "&:before": {
-        background: theme.palette.primary.main
+const useStyles = makeStyles(
+  theme => ({
+    box: {
+      "&$checked": {
+        "&:before": {
+          background: theme.palette.primary.main,
+          color: theme.palette.background.paper,
+          content: '"\\2713"',
+          fontWeight: "bold",
+          textAlign: "center"
+        },
+        borderColor: theme.palette.primary.main
       },
-      borderColor: theme.palette.primary.main
-    },
-    "&$disabled": {
-      borderColor: theme.palette.grey[200]
-    },
-    "&$indeterminate": {
-      "&:before": {
-        background: theme.palette.primary.main,
-        height: 2,
-        top: 5
+      "&$disabled": {
+        borderColor: theme.palette.grey[200]
       },
-      borderColor: theme.palette.primary.main
+      "&$indeterminate": {
+        "&:before": {
+          background: theme.palette.primary.main,
+          height: 2,
+          top: 5
+        },
+        borderColor: theme.palette.primary.main
+      },
+      "&:before": {
+        background: "rgba(0, 0, 0, 0)",
+        content: '""',
+        height: 14,
+        left: -1,
+        position: "absolute",
+        top: -1,
+        transition: theme.transitions.duration.short + "ms",
+        width: 14
+      },
+
+      WebkitAppearance: "none",
+      border: `1px solid ${theme.palette.action.active}`,
+      boxSizing: "border-box",
+      cursor: "pointer",
+      height: 14,
+      outline: "0",
+      position: "relative",
+      userSelect: "none",
+      width: 14
     },
-    "&:before": {
-      background: "rgba(0, 0, 0, 0)",
-      content: '""',
-      height: 8,
-      left: 2,
-      position: "absolute",
-      top: 2,
-      transition: theme.transitions.duration.short + "ms",
-      width: 8
-    },
-    WebkitAppearance: "none",
-    border: `1px solid ${theme.palette.grey[500]}`,
-    boxSizing: "border-box",
-    cursor: "pointer",
-    height: 14,
-    outline: "0",
-    position: "relative",
-    userSelect: "none",
-    width: 14
-  },
-  checked: {},
-  disabled: {},
-  indeterminate: {},
-  root: {
-    alignItems: "center",
-    borderRadius: "100% !important",
-    cursor: "pointer",
-    display: "flex",
-    height: 48,
-    justifyContent: "center",
-    width: 48
-  }
-}));
+    checked: {},
+    disabled: {},
+    indeterminate: {},
+    root: {
+      "&:hover": {
+        background: fade(theme.palette.primary.main, 0.1)
+      },
+      alignSelf: "start",
+      borderRadius: "100%",
+      cursor: "pointer",
+      display: "flex",
+      height: 30,
+      justifyContent: "center",
+      margin: "5px 9px",
+      width: 30
+    }
+  }),
+  { name: "Checkbox" }
+);
 const Checkbox: React.FC<CheckboxProps> = props => {
   const {
     checked,
     className,
+
     disabled,
+    disableClickPropagation,
     indeterminate,
     onChange,
-    onClick,
     value,
     name,
     ...rest
   } = props;
   const classes = useStyles(props);
+
   const inputRef = React.useRef<HTMLInputElement>(null);
+  const handleClick = React.useCallback(
+    disableClickPropagation
+      ? event => {
+          event.stopPropagation();
+          inputRef.current.click();
+        }
+      : () => inputRef.current.click(),
+    []
+  );
 
   return (
     <ButtonBase
@@ -92,7 +115,7 @@ const Checkbox: React.FC<CheckboxProps> = props => {
       centerRipple
       className={classNames(classes.root, className)}
       disabled={disabled}
-      onClick={stopPropagation(() => inputRef.current.click())}
+      onClick={handleClick}
     >
       <input
         className={classNames(classes.box, {
