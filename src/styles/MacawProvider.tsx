2a652339b87da4d1ff5be6409e7d5a8e5298d913
dominik-zeglen <flesz3@o2.pl>
2020-02-04 12:45:07 +0100

    Add helmet support

--- src/styles/MacawProvider.tsx
+++ src/styles/MacawProvider.tsx
@@ -1,21 +1,29 @@
 import MuiThemeProvider from "@material-ui/styles/ThemeProvider";
 import React from "react";
+import Helmet from "react-helmet";
 
 import { AppLayoutContextProvider } from "../AppLayout/AppLayoutContext";
 import Baseline from "./Baseline";
 import createMuiTheme, { MacawTheme } from "./createTheme";
 
 interface MacawThemeProviderProps {
+  meta?: boolean;
   theme: MacawTheme;
 }
 const MacawThemeProvider: React.FC<MacawThemeProviderProps> = ({
   children,
+  meta,
   theme
 }) => {
   const [isMenuExpanded, setMenuExpansionState] = React.useState(true);
 
   return (
     <MuiThemeProvider theme={createMuiTheme(theme)}>
+      {meta && (
+        <Helmet>
+          <meta name="theme-color" content={theme.background.default} />
+        </Helmet>
+      )}
       <Baseline />
       <AppLayoutContextProvider
         menu={{
@@ -30,4 +38,7 @@ const MacawThemeProvider: React.FC<MacawThemeProviderProps> = ({
 };
 
 MacawThemeProvider.displayName = "MacawThemeProvider";
+MacawThemeProvider.defaultProps = {
+  meta: true
+};
 export default MacawThemeProvider;
