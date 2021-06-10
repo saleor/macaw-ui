d9b4928a43dfea51761988c81797d2a8d3738c50
Dominik Żegleń <flesz3@o2.pl>
2021-06-10 15:51:02 +0200

    Add alerts

---
+++ src/Alert/Alert.tsx
@@ -0,0 +1,85 @@
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import IconButton from "@material-ui/core/IconButton";
+import Typography from "@material-ui/core/Typography";
+import CloseIcon from "@material-ui/icons/Close";
+import clsx from "clsx";
+import React from "react";
+import SVG from "react-inlinesvg";
+
+import warningIcon from "../assets/alert_icon.svg";
+import errorIcon from "../assets/error_icon.svg";
+import infoIcon from "../assets/info_icon.svg";
+import successIcon from "../assets/success_icon.svg";
+import useStyles from "./styles";
+
+export type AlertVariant = "error" | "warning" | "success" | "info";
+export interface AlertProps {
+  className?: string;
+  close?: boolean;
+  variant: AlertVariant;
+  title: string;
+}
+
+function getVariantIcon(variant: AlertVariant): string {
+  switch (variant) {
+    case "error":
+      return errorIcon;
+    case "success":
+      return successIcon;
+    case "warning":
+      return warningIcon;
+  }
+  return infoIcon;
+}
+
+const Alert: React.FC<AlertProps> = ({
+  className,
+  close = true,
+  variant = "info",
+  title,
+  children,
+}) => {
+  const classes = useStyles();
+  const [visible, setVisible] = React.useState(true);
+
+  if (!visible) {
+    return null;
+  }
+
+  return (
+    <Card
+      elevation={0}
+      className={clsx(className, classes.root, {
+        [classes.error]: variant === "error",
+        [classes.warning]: variant === "warning",
+        [classes.success]: variant === "success",
+      })}
+    >
+      <CardContent>
+        <div className={classes.container}>
+          <div>
+            <SVG className={classes.icon} src={getVariantIcon(variant)} />
+          </div>
+          <div className={classes.content}>
+            <div className={classes.titleBar}>
+              <Typography variant="h5">{title}</Typography>
+              {close && (
+                <IconButton
+                  className={classes.close}
+                  onClick={() => setVisible(false)}
+                >
+                  <CloseIcon />
+                </IconButton>
+              )}
+            </div>
+            {children}
+          </div>
+        </div>
+      </CardContent>
+    </Card>
+  );
+};
+
+Alert.displayName = "Alert";
+export default Alert;
---
+++ src/Alert/styles.ts
@@ -0,0 +1,40 @@
+import { makeStyles } from "../theme";
+
+const useStyles = makeStyles(
+  (theme) => ({
+    close: {
+      color: theme.palette.common.black,
+      position: "absolute",
+      right: theme.spacing(-2),
+      top: theme.spacing(-1),
+    },
+    container: {
+      columnGap: theme.spacing(2) + "px",
+      display: "grid",
+      gridTemplateColumns: "40px 1fr",
+    },
+    content: {
+      padding: theme.spacing(0, 1),
+    },
+    icon: {},
+    root: {},
+    titleBar: {
+      margin: theme.spacing(0.5, 0),
+      position: "relative",
+    },
+    error: {
+      background: theme.palette.alert.paper.error,
+    },
+    warning: {
+      background: theme.palette.alert.paper.warning,
+    },
+    success: {
+      background: theme.palette.alert.paper.success,
+    },
+  }),
+  {
+    name: "Alert",
+  }
+);
+
+export default useStyles;
