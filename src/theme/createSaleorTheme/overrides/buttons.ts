789296806fd10da73547e217ffa86fce21a9912a
Dominik Żegleń <flesz3@o2.pl>
2021-07-14 17:10:45 +0200

    Improve button styles

--- src/theme/createSaleorTheme/overrides/buttons.ts
+++ src/theme/createSaleorTheme/overrides/buttons.ts
@@ -3,92 +3,119 @@ import type { Overrides } from "@material-ui/core/styles/overrides";
 
 import { SaleorThemeColors } from "../types";
 
-export const buttonOverrides = (colors: SaleorThemeColors): Overrides => ({
-  MuiButton: {
-    contained: {
-      "&$disabled": {
-        backgroundColor: fade(colors.primary, 0.12),
-        color: "#FFFFFF",
-      },
-      "&:active": {
+export const buttonOverrides = (colors: SaleorThemeColors): Overrides => {
+  const containedPrimaryStates = {
+    "&&:active": {
+      backgroundColor: fade(colors.primary, 0.4),
+    },
+    "&&:hover": {
+      backgroundColor: fade(colors.primary, 0.6),
+    },
+  };
+  const containedSecondaryStates = {
+    "&&:active": {
+      backgroundColor: fade(colors.primary, 0.2),
+    },
+    "&&:hover": {
+      backgroundColor: fade(colors.primary, 0.1),
+    },
+  };
+
+  return {
+    MuiButton: {
+      contained: {
+        "&$disabled": {
+          backgroundColor: fade(colors.primary, 0.12),
+          color: "#FFFFFF",
+        },
+        "&:active": {
+          boxShadow: "none",
+        },
+        "&:hover": {
+          "@media(hover: none)": {
+            boxShadow: "none",
+          },
+          boxShadow: "none",
+        },
         boxShadow: "none",
       },
-      "&:hover": {
-        boxShadow: "none",
+      containedPrimary: {
+        ...containedPrimaryStates,
+        "@media(hover: none)": containedPrimaryStates,
       },
-      boxShadow: "none",
-    },
-    containedPrimary: {
-      "&:active": {
-        backgroundColor: darken(colors.primary, 0.4),
-      },
-      "&:hover": {
-        backgroundColor: darken(colors.primary, 0.1),
-      },
-    },
-    label: {
-      fontWeight: 600,
-    },
-    root: {
-      "& svg": {
-        marginLeft: 8,
-      },
-      borderRadius: 4,
-    },
-    textPrimary: {
-      "&:not($disabled) span": {
+      containedSecondary: {
+        ...containedSecondaryStates,
+        "@media(hover: none)": containedSecondaryStates,
         color: colors.primary,
       },
-    },
-    textSizeSmall: {
-      fontSize: "1.3rem",
-    },
-  },
-  MuiIconButton: {
-    root: {
-      "&:hover": {
-        backgroundColor: fade(colors.primary, 0.12),
+      label: {
+        fontWeight: 600,
       },
-    },
-  },
-  MuiSwitch: {
-    colorPrimary: {
-      "&$checked": {
-        color: colors.background.paper,
+      root: {
+        "& svg": {
+          marginLeft: 8,
+        },
+        borderRadius: 4,
       },
-    },
-    root: {
-      "&$disabled": {
-        "&$switchBase": {
-          "& + $thumb": {
-            backgroundColor: colors.gray.disabled,
-          },
+      text: {
+        ...containedSecondaryStates,
+        "@media(hover: none)": containedSecondaryStates,
+      },
+      textPrimary: {
+        "&:not($disabled) span": {
+          color: colors.primary,
         },
       },
-      height: 48,
-      width: 72,
-    },
-    switchBase: {
-      "&$checked": {
-        transform: "translateX(23px)",
+      textSizeSmall: {
+        fontSize: "1.3rem",
       },
-      boxShadow: "none",
-      left: 1,
-      marginLeft: 4,
-      top: 5,
     },
-    thumb: {
-      boxShadow: "none",
-    },
-    track: {
-      "$colorPrimary$checked + &": {
-        backgroundColor: colors.primary,
+    MuiIconButton: {
+      root: {
+        "&:hover": {
+          backgroundColor: fade(colors.primary, 0.12),
+        },
       },
-      backgroundColor: colors.gray.default,
-      borderRadius: 12,
-      height: 24,
-      opacity: [["1"], "!important"] as any,
-      width: 48,
     },
-  },
-});
+    MuiSwitch: {
+      colorPrimary: {
+        "&$checked": {
+          color: colors.background.paper,
+        },
+      },
+      root: {
+        "&$disabled": {
+          "&$switchBase": {
+            "& + $thumb": {
+              backgroundColor: colors.gray.disabled,
+            },
+          },
+        },
+        height: 48,
+        width: 72,
+      },
+      switchBase: {
+        "&$checked": {
+          transform: "translateX(23px)",
+        },
+        boxShadow: "none",
+        left: 1,
+        marginLeft: 4,
+        top: 5,
+      },
+      thumb: {
+        boxShadow: "none",
+      },
+      track: {
+        "$colorPrimary$checked + &": {
+          backgroundColor: colors.primary,
+        },
+        backgroundColor: colors.gray.default,
+        borderRadius: 12,
+        height: 24,
+        opacity: [["1"], "!important"] as any,
+        width: 48,
+      },
+    },
+  };
+};
