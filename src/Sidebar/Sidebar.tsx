32ad87cdcf0fafa480124c4ab4866cdbaf917d20
Dominik Żegleń <flesz3@o2.pl>
2021-07-16 10:47:14 +0200

    Update sidebar styles

--- src/Sidebar/Sidebar.tsx
+++ src/Sidebar/Sidebar.tsx
@@ -12,7 +12,7 @@ import { BaseSidebarProps } from "./types";
 const useStyles = makeStyles(
   (theme) => ({
     expandButton: {
-      marginLeft: theme.spacing(2),
+      marginLeft: theme.spacing(1.5),
     },
     float: {
       height: "100vh",
@@ -21,7 +21,7 @@ const useStyles = makeStyles(
       overflowY: "auto",
     },
     logo: {
-      margin: `36px 0 ${theme.spacing(3)} ${theme.spacing(3.5)}`,
+      margin: `36px 0 ${theme.spacing(3)} ${theme.spacing(2.5)}`,
     },
     root: {
       transition: "width 0.5s ease",
@@ -32,7 +32,7 @@ const useStyles = makeStyles(
       width: shrunkMenuWidth,
     },
     toolbarContainer: {
-      margin: theme.spacing(1, 0, 1, 2),
+      margin: theme.spacing(1, 0, 1, 1.5),
     },
   }),
   {
