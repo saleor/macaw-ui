8098d99863ce14fdbec8c81da4d708bd9323bf31
Dominik Żegleń <flesz3@o2.pl>
2021-07-20 17:00:51 +0200

    Remove react-helmet

---
+++ src/theme/utils.ts
@@ -0,0 +1,13 @@
+export function changeColorMeta(color: string) {
+  const themeColorTag = document.createElement("meta");
+  themeColorTag.setAttribute("content", color);
+  themeColorTag.setAttribute("name", "theme-color");
+
+  const existingColorTag = document.head.querySelector(
+    `meta[name="theme-color"]`
+  );
+  if (existingColorTag) {
+    existingColorTag.remove();
+  }
+  document.head.appendChild(themeColorTag);
+}
