d8ba9ff3fc8e547645d3a0bea64771f3c241418e
Dominik Żegleń <flesz3@o2.pl>
2021-07-21 17:03:45 +0200

    Improve tab styles

--- src/PageTabs/styles.ts
+++ src/PageTabs/styles.ts
@@ -8,10 +8,12 @@ const useStyles = makeStyles(
       },
       "&.Mui-selected": {
         background: theme.palette.secondary.main,
+        color: theme.palette.primary.main,
       },
       borderRadius: 8,
       minHeight: 40,
       padding: theme.spacing(1, 2),
+      textTransform: "unset",
     },
     containerRoot: {
       minHeight: "unset",
