7ed4a18d05184c1e88e1eca4aaf72c1ec509b480
Dawid Tarasiuk <tarasiukdawid@gmail.com>
2021-09-17 17:41:41 +0300

    Update icons props

--- src/icons/CompleteIcon.tsx
+++ src/icons/CompleteIcon.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 
-export const CompleteIcon: React.FC = () => (
+export const CompleteIcon: React.FC<React.SVGProps<SVGSVGElement>> = (
+  props
+) => (
   <svg
     width="40"
     height="40"
     viewBox="0 0 40 40"
     fill="none"
     xmlns="http://www.w3.org/2000/svg"
+    {...props}
   >
     <circle cx="20" cy="20" r="20" fill="white" />
     <circle cx="20" cy="20" r="14" stroke="#5DC292" stroke-width="4" />
--- src/icons/WarningIcon.tsx
+++ src/icons/WarningIcon.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 
-export const WarningIcon: React.FC = () => (
+export const WarningIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
   <svg
     width="40"
     height="40"
     viewBox="0 0 40 40"
     fill="none"
     xmlns="http://www.w3.org/2000/svg"
+    {...props}
   >
     <circle cx="20" cy="20" r="20" fill="white" />
     <circle cx="20" cy="20" r="14" stroke="#FFB84E" stroke-width="4" />
