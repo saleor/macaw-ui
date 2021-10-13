d7640efe177e7e8fc867ab3f9e54558fa81ecbb3
Dominik Żegleń <flesz3@o2.pl>
2021-10-13 11:28:59 +0200

    Add clickable pill component

--- src/PillLink/PillLink.tsx
+++ src/PillLink/PillLink.tsx
@@ -1,5 +1,34 @@
+import clsx from "clsx";
 import React from "react";
 
-export type PillLinkProps = HTMLButtonElement;
+import useStyles from "./styles";
 
-export const PillLink: React.FC<PillLinkProps> = { children };
+export interface PillLinkProps
+  extends React.DetailedHTMLProps<
+    React.AnchorHTMLAttributes<HTMLAnchorElement>,
+    HTMLAnchorElement
+  > {
+  state?: "default" | "hover" | "active";
+}
+
+export const PillLink: React.FC<PillLinkProps> = ({
+  children,
+  className,
+  state = "default",
+  ...props
+}) => {
+  const classes = useStyles();
+
+  return (
+    <a
+      className={clsx(classes.root, className, {
+        [classes.hover]: state === "hover",
+        [classes.active]: state === "active",
+      })}
+      {...props}
+    >
+      {children}
+    </a>
+  );
+};
+PillLink.displayName = "PillLink";
