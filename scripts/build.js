26185fb722f9c32667e6aefcfab286b69195f145
Dominik Żegleń <flesz3@o2.pl>
2021-10-14 12:06:39 +0200

    Use "concurrently"

--- scripts/build.js
+++ scripts/build.js
@@ -1,39 +1,12 @@
-const chalk = require("chalk");
-const tsc = require("./tsc");
 const esbuild = require("./esbuild");
 
-const log = console.log;
 const entryPoints = ["./src/index.tsx"];
-
-const printLog = (scope, msg, lvl) => {
-  let color;
-  switch (lvl) {
-    case "err":
-      color = chalk.red;
-      break;
-    case "warn":
-      color = chalk.yellow;
-      break;
-    default:
-      color = chalk.blue;
-  }
-
-  log(`${color(scope)} ${msg}`);
-};
-
-const esbLog = (msg, lvl) => printLog("ESB", msg, lvl);
-const tscLog = (msg, lvl) => printLog("TSC", msg, lvl);
-
 const watch = process.argv[2] === "watch";
 
 if (watch) {
-  esbuild(entryPoints, esbLog, watch);
-  tsc(entryPoints, tscLog, watch);
+  esbuild(entryPoints, watch);
 } else {
-  esbuild(entryPoints, esbLog, watch)
-    .then(() => {
-      return tsc(entryPoints, tscLog, watch);
-    })
+  esbuild(entryPoints, watch)
     .then(() => process.exit(0))
     .catch(() => process.exit(1));
 }
