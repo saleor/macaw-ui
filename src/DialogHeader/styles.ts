dc6353205d4c80fe4207b17316406a37a7df4bde
Dominik Żegleń <flesz3@o2.pl>
2021-10-15 11:36:23 +0200

    Fix dialog close icon

--- src/DialogHeader/styles.ts
+++ src/DialogHeader/styles.ts
@@ -1,9 +1,18 @@
 import { makeStyles } from "../theme";
 
 export const useStyles = makeStyles(
-  () => ({
+  (theme) => ({
     button: {
-      opacity: 0.6,
+      "&:hover, &:focus": {
+        color: theme.palette.saleor.active[1],
+      },
+      appearance: "none",
+      border: "none",
+      background: "transparent",
+      color: theme.palette.saleor.main[3],
+      cursor: "pointer",
+      padding: 0,
+      outline: 0,
     },
     wrapper: {
       alignItems: "center",
