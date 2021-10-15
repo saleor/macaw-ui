412e6c8658f9f447ad1126e207377e49a8af3f32
Dominik Żegleń <flesz3@o2.pl>
2021-10-15 11:48:51 +0200

    Improve layout button styles

--- src/LayoutButton/LayoutButton.tsx
+++ src/LayoutButton/LayoutButton.tsx
@@ -1,16 +1,30 @@
 import type { ButtonBaseProps } from "@material-ui/core/ButtonBase";
+
 import ButtonBase from "@material-ui/core/ButtonBase";
 import clsx from "clsx";
 import React from "react";
 
 import useStyles from "./styles";
 
-export const LayoutButton: React.FC<ButtonBaseProps> = React.forwardRef(
-  ({ className, children, ...rest }, ref) => {
+export type LayoutButtonProps<T extends React.ElementType = "button"> =
+  ButtonBaseProps<T> & {
+    state?: "default" | "hover" | "active";
+  };
+
+export const LayoutButton: React.FC<LayoutButtonProps> = React.forwardRef(
+  ({ className, children, state = "default", ...rest }, ref) => {
     const classes = useStyles();
 
     return (
-      <ButtonBase className={clsx(classes.root, className)} ref={ref} {...rest}>
+      <ButtonBase
+        className={clsx(classes.root, className, {
+          [classes.hover]: state === "hover",
+          [classes.active]: state === "active",
+        })}
+        disableRipple
+        ref={ref}
+        {...rest}
+      >
         {children}
       </ButtonBase>
     );
