de30f03ed62b16f4bc59a4c5b115eec62432e2dc
Dominik Żegleń <flesz3@o2.pl>
2021-10-15 11:48:51 +0200

    Improve layout button styles

--- src/LayoutButton/LayoutButton.stories.tsx
+++ src/LayoutButton/LayoutButton.stories.tsx
@@ -1,9 +1,29 @@
 import { Meta, Story } from "@storybook/react";
 import React from "react";
+import { makeStyles } from "..";
 
 import { LayoutButton } from "./LayoutButton";
 
-export const Default: Story = () => <LayoutButton>Lorem Ipsum</LayoutButton>;
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: "flex",
+    alignItems: "flex-start",
+    flexDirection: "column",
+    rowGap: theme.spacing(3),
+  },
+}));
+
+export const Default: Story = () => {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <LayoutButton>Lorem Ipsum</LayoutButton>
+      <LayoutButton state="hover">Lorem Ipsum</LayoutButton>
+      <LayoutButton state="active">Lorem Ipsum</LayoutButton>
+    </div>
+  );
+};
 
 export default {
   title: "Layout Button",
--- src/LayoutButton/styles.ts
+++ src/LayoutButton/styles.ts
@@ -5,8 +5,12 @@ import { makeStyles } from "../theme";
 const useStyles = makeStyles(
   (theme) => ({
     root: {
-      "&:hover": {
+      "&:hover, &$hover": {
         background: theme.palette.background.paper,
+        color: theme.palette.primary.main,
+      },
+      "&:active, &$active": {
+        background: theme.palette.saleor.active[4],
         color: theme.palette.primary.main,
       },
       background: fade(theme.palette.background.paper, 0.4),
@@ -18,8 +22,10 @@ const useStyles = makeStyles(
       padding: theme.spacing(0.5, 2),
       textAlign: "center",
       textTransform: "uppercase",
-      transition: theme.transitions.duration.standard + "ms",
+      transition: theme.transitions.duration.shorter + "ms",
     },
+    hover: {},
+    active: {},
   }),
   { name: "LayoutButton" }
 );
