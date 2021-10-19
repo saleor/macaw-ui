12a4ae11d72b31abc717f96c87e5adf58bcfd2e3
Dominik Żegleń <flesz3@o2.pl>
2021-10-19 10:43:48 +0200

    Add className to chip

--- src/StatusChip/types.ts
+++ src/StatusChip/types.ts
@@ -1,6 +1,7 @@
 export type StatusChipSize = "md" | "lg";
 export interface StatusChipProps {
   children?: null;
+  className?: string;
   label: string;
   size?: StatusChipSize;
   variant: "success" | "warning" | "neutral" | "error";
