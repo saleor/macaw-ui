bdb157c8fd93c4d74e959981fb82bdb8f1f1e497
Dominik Żegleń <flesz3@o2.pl>
2021-10-25 16:04:59 +0200

    Remove margin in icons

--- src/theme/createSaleorTheme/overrides/buttons.ts
+++ src/theme/createSaleorTheme/overrides/buttons.ts
@@ -45,9 +45,6 @@ export const buttonOverrides = (colors: SaleorThemeColors): Overrides => {
         fontWeight: 600,
       },
       root: {
-        "& svg": {
-          marginLeft: 8,
-        },
         borderRadius: 4,
         fontSize: "1.6rem",
         letterSpacing: "0.06rem",
