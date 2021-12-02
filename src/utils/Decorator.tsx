880660fc600cb749573e7ca0613ea4c125b032fb
Dominik Żegleń <flesz3@o2.pl>
2021-12-02 11:39:37 +0100

    Fix module (#91)
    * Output cjs
    
    * Build cjs and esm
    
    * Add node platrofm
    
    * Update size-limit config
    
    * Fix module path

--- src/utils/Decorator.tsx
+++ src/utils/Decorator.tsx
@@ -1,11 +1,10 @@
 import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
-import { StoryFn } from "@storybook/addons";
 import React from "react";
 
 import { ThemeProvider } from "../theme";
 
-export const Decorator = (storyFn: StoryFn<React.ReactNode>) => (
+export const Decorator = (storyFn: any) => (
   <ThemeProvider>
     <div
       style={{
@@ -17,7 +16,7 @@ export const Decorator = (storyFn: StoryFn<React.ReactNode>) => (
   </ThemeProvider>
 );
 
-export const GuideDecorator = (storyFn: StoryFn<React.ReactNode>) => (
+export const GuideDecorator = (storyFn: any) => (
   <Card style={{ margin: "auto", width: 700 }}>
     <CardContent>{storyFn()}</CardContent>
   </Card>
