1f5d2daafde49779ce8a4a7f738e06d0fb8eec19
Dominik Żegleń <flesz3@o2.pl>
2021-12-03 12:43:01 +0100

    Add navigation card

---
+++ src/NavigationCard/NavigationCard.tsx
@@ -0,0 +1,42 @@
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import Typography from "@material-ui/core/Typography";
+import React from "react";
+
+import useStyles from "./styles";
+
+export interface NavigationCardProps {
+  title: string;
+  description: string;
+  icon: React.ReactNode;
+}
+
+export const NavigationCard: React.FC<NavigationCardProps> = ({
+  icon,
+  title,
+  description,
+}) => {
+  const classes = useStyles();
+
+  return (
+    <Card className={classes.card}>
+      <CardContent className={classes.cardContent}>
+        <div className={classes.content}>
+          <div className={classes.icon}>{icon}</div>
+          <div>
+            <Typography variant="subtitle1" className={classes.boxLinkTitle}>
+              {title}
+            </Typography>
+            <Typography
+              variant="body2"
+              color="textSecondary"
+              className={classes.boxLinkText}
+            >
+              {description}
+            </Typography>
+          </div>
+        </div>
+      </CardContent>
+    </Card>
+  );
+};
