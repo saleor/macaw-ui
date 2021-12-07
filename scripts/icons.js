af8b0009036257b866c8ae153c86070b231db577
Dominik Żegleń <flesz3@o2.pl>
2021-12-07 13:59:17 +0100

    Add script to build icons index file

---
+++ scripts/icons.js
@@ -0,0 +1,18 @@
+const fs = require("fs");
+const path = require("path");
+
+const dir = path.resolve(__dirname, "../src/icons");
+const iconRegexp = new RegExp(/.*Icon\.tsx/);
+
+const files = fs.readdirSync(dir, {
+  withFileTypes: true,
+});
+
+console.log(files);
+const imports = files
+  .map((file) => file.name)
+  .filter((fileName) => iconRegexp.test(fileName))
+  .map((fileName) => fileName.replace(".tsx", ""))
+  .reduce((acc, fileName) => acc + `export * from "./${fileName}"\n`, "");
+
+fs.writeFileSync(path.join(dir, "index.ts"), imports);
