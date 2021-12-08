f82badd98360352ddcecd34799a107bea79dd053
Dominik Żegleń <flesz3@o2.pl>
2021-12-08 15:16:09 +0100

    Add dark palette

--- src/theme/themes.ts
+++ src/theme/themes.ts
@@ -1,20 +1,18 @@
-import { fade } from "@material-ui/core/styles/colorManipulator";
-
 import { SaleorThemeColors } from "./createSaleorTheme";
 
 export const dark: SaleorThemeColors = {
   alert: {
     paper: {
-      error: "#E0444E",
-      info: "#2E2F31",
-      success: "#5DC292",
-      warning: "#E29A2E",
+      error: "#B65757",
+      info: "rgba(250, 250, 250, 1)",
+      success: "rgba(67, 156, 123, 1)",
+      warning: "rgba(183, 138, 72, 1)",
     },
     icon: {
       error: "#FE6E76",
       info: "#FAFAFA",
-      success: "#5DC292",
-      warning: "#FFB84E",
+      success: "#A4E8BA",
+      warning: "#FFE6AB",
     },
   },
   highlightInactive: {
@@ -22,39 +20,31 @@ export const dark: SaleorThemeColors = {
   },
   autofill: "#5D5881",
   background: {
-    default: "#1D1E1F",
-    paper: "#2E2F31",
+    default: "#222431",
+    paper: "#34384B",
   },
   checkbox: {
     default: "#FFFFFF",
   },
   divider: "#252728",
-  error: "#C22D74",
-  font: {
-    button: "#202124",
-    default: "#FCFCFC",
-    gray: "#9E9D9D",
-    textButton: "#FFFFFF",
-    textDisabled: "#FCFCFC",
-  },
   gray: {
     default: "#202124",
     disabled: "rgba(32, 33, 36, 0.6)",
   },
 
   active: {
-    1: "#056DFF",
-    2: "#378AFF",
-    3: "#68A7FF",
-    4: "#C1DBFF",
-    5: "#E6F0FF",
+    1: "#3EA3FF",
+    2: "#388AD6",
+    3: "#3371AD",
+    4: "#2D5783",
+    5: "#283E5A",
   },
   main: {
-    1: "#FCFCFC",
-    2: "rgba(252, 252, 252, 0.8)",
-    3: "rgba(252, 252, 252, 0.6)",
-    4: "rgba(252, 252, 252, 0.4)",
-    5: "rgba(252, 252, 252, 0.1)",
+    1: "#FAFAFA",
+    2: "rgba(250, 250, 250, 0.8)",
+    3: "rgba(250, 250, 250, 0.6)",
+    4: "rgba(250, 250, 250, 0.4)",
+    5: "rgba(250, 250, 250, 0.1)",
   },
   fail: {
     dark: "#B65757",
@@ -62,17 +52,15 @@ export const dark: SaleorThemeColors = {
     light: "#FEDEDE",
   },
   errorAction: {
-    1: "#B63755",
-    2: "#D36474",
-    3: "#E9878B",
-    4: "#F7B6B2",
-    5: "#FBDDD8",
+    1: "#EB2B59",
+    2: "rgba(235, 43, 89, 0.8)",
+    3: "rgba(235, 43, 89, 0.6)",
+    4: "rgba(235, 43, 89, 0.4)",
+    5: "rgba(235, 43, 89, 0.2)",
   },
 
-  disabled: "#CCDDDD",
+  disabled: "#5D6D83",
   paperBorder: "#252728",
-  primary: "#13BEBB",
-  secondary: "#21125E",
   success: "#5DC292",
   theme: "dark",
 };
@@ -103,14 +91,6 @@ export const light: SaleorThemeColors = {
     default: "#616161",
   },
   divider: "#EAEAEA",
-  error: "#FE6D76",
-  font: {
-    button: "#FFFFFF",
-    default: "#28234A",
-    gray: fade("#28234A", 0.6),
-    textButton: "#06847B",
-    textDisabled: fade("#28234A", 0.4),
-  },
   gray: {
     default: "#C8C8C8",
     disabled: "#C0CFE2",
@@ -145,8 +125,6 @@ export const light: SaleorThemeColors = {
 
   disabled: "#CCDDDD",
   paperBorder: "#EAEAEA",
-  primary: "#056DFF",
-  secondary: "#FFFFFF",
   success: "#5DC292",
   theme: "light",
 };
