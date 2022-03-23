968b22e8c29ee247f3b3110dc92d264e59334005
Wojciech <wojciech.mista@hotmail.com>
2022-03-23 10:54:53 +0100

    Refactor switch selector

--- src/SwitchSelector/SwitchSelector.tsx
+++ src/SwitchSelector/SwitchSelector.tsx
@@ -1,17 +1,5 @@
-import clsx from "clsx";
 import React from "react";
-import { Button, makeStyles } from "..";
-
-type SwitchSelectorOptions = {
-  label: string | React.ReactNode;
-  value: string;
-};
-
-interface SwitchSelectorProps {
-  options: SwitchSelectorOptions[];
-  setActiveTab: (index: string) => void;
-  activeTab?: string;
-}
+import { makeStyles } from "..";
 
 const useStyles = makeStyles((theme) => ({
   switchSelector: {
@@ -19,50 +7,13 @@ const useStyles = makeStyles((theme) => ({
     padding: "6px",
     borderRadius: 4,
     width: "fit-content",
-    "&:not(:first-child)": {
-      marginLeft: "6px",
-    },
-  },
-  button: {
-    color: "#77738C",
-    background: "none",
-    borderColor: "transparent",
-    cursor: "pointer",
-
-    "&:hover": {
-      backgroundColor: theme.palette.common.white,
-      color: theme.palette.saleor.active[1],
-      border: `1px solid ${theme.palette.saleor.active[1]}`,
-    },
-  },
-  buttonActive: {
-    color: theme.palette.common.white,
-    backgroundColor: theme.palette.saleor.active[1],
   },
 }));
 
-export const SwitchSelector: React.FC<SwitchSelectorProps> = ({
-  options,
-  setActiveTab,
-  activeTab,
-}) => {
+export const SwitchSelector: React.FC<{}> = ({ children }) => {
   const classes = useStyles();
 
-  return (
-    <div className={classes.switchSelector}>
-      {options.map(({ label, value }) => (
-        <Button
-          key={value}
-          onClick={() => setActiveTab(value)}
-          className={clsx(classes.button, {
-            [classes.buttonActive]: value === activeTab,
-          })}
-        >
-          {label}
-        </Button>
-      ))}
-    </div>
-  );
+  return <div className={classes.switchSelector}>{children}</div>;
 };
 
 SwitchSelector.displayName = "SwitchSelector";
