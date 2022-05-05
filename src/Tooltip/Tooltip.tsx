ef3e4bf2fd04f66f2da1a46dc8fa3f74787491e1
Jonatan Witoszek <jonatanwitoszek@gmail.com>
2022-05-05 17:55:03 +0200

    Update Tooltip to new design

--- src/Tooltip/Tooltip.tsx
+++ src/Tooltip/Tooltip.tsx
@@ -1,28 +1,222 @@
-import MUITooltip from "@material-ui/core/Tooltip";
-import { TooltipProps as MUITooltipProps } from "@material-ui/core/Tooltip";
-import React from "react";
+import {
+  AlignedPlacement,
+  arrow,
+  autoUpdate,
+  flip,
+  FloatingPortal,
+  offset,
+  Placement,
+  shift,
+  Side,
+  useDismiss,
+  useFloating,
+  useFocus,
+  useHover,
+  useInteractions,
+  useRole,
+} from "@floating-ui/react-dom-interactions";
+import clsx from "clsx";
+import React, { useEffect, useLayoutEffect, useMemo } from "react";
+import { useCallback } from "react";
+import { useRef } from "react";
+import { useState } from "react";
 
+import { useTheme } from "../theme";
 import useStyles from "./styles";
 
-export interface TooltipProps extends MUITooltipProps {
-  variant?: "info" | "success" | "warning" | "error";
+export interface TooltipProps {
+  variant?: "success" | "warning" | "error";
+  placement?: Placement;
+  arrow?: boolean;
+  onClick?: React.MouseEventHandler<any>;
+  onOpen?: () => void;
+  onClose?: () => void;
+  open?: boolean;
+  initialOpen?: boolean;
   disabled?: boolean;
+  children: React.ReactElement;
+  header?: string;
+  title?: React.ReactNode;
 }
 
-export const Tooltip: React.FC<TooltipProps> = ({
-  children,
-  disabled = false,
-  variant = "info",
-  ...rest
-}) => {
-  const classes = useStyles({ variant, children, ...rest });
+const mapSide: Record<Side, Side> = {
+  top: "bottom",
+  right: "left",
+  bottom: "top",
+  left: "right",
+} as const;
 
-  return disabled ? (
-    children
-  ) : (
-    <MUITooltip classes={classes} {...rest}>
-      {children}
-    </MUITooltip>
+export const Tooltip: React.FC<TooltipProps> = ({
+  variant,
+  placement: initialPlacement = "bottom",
+  arrow: hasArrow = true,
+  onOpen,
+  onClose,
+  onClick,
+  open: forceState,
+  initialOpen = false,
+  disabled = false,
+  children,
+  header,
+  title,
+}) => {
+  const classes = useStyles({ variant });
+  const { themeType } = useTheme();
+
+  const [stateOpen, setStateOpen] = useState(initialOpen);
+  const [isMountedReference, setIsMountedReference] = useState(false);
+  const [isMountedFloating, setIsMountedFloating] = useState(false);
+  const arrowRef = useRef<HTMLSpanElement | null>(null);
+
+  const open = forceState ?? stateOpen;
+
+  const {
+    x,
+    y,
+    placement,
+    strategy,
+    context,
+    refs,
+    update,
+    middlewareData: { arrow: { x: arrowX, y: arrowY } = {} },
+  } = useFloating({
+    placement: initialPlacement,
+    open,
+    onOpenChange: (isOpen) => {
+      setStateOpen(isOpen);
+
+      if (isOpen) {
+        onOpen?.();
+      } else {
+        onClose?.();
+      }
+    },
+    middleware: [
+      offset(5),
+      flip(),
+      shift({ padding: 12 }),
+      arrow({ element: arrowRef }),
+    ],
+  });
+
+  const { getReferenceProps, getFloatingProps } = useInteractions([
+    useHover(context),
+    useFocus(context),
+    useRole(context, { role: "tooltip" }),
+    useDismiss(context),
+  ]);
+
+  useEffect(() => {
+    if (!refs.reference.current || !refs.floating.current || !open) {
+      return;
+    }
+    return autoUpdate(refs.reference.current, refs.floating.current, update);
+  }, [isMountedFloating, isMountedReference, update, open]);
+
+  const side = useMemo<Side>(() => {
+    return placement.split("-")[0] as Side;
+  }, [placement]);
+
+  const swappedSide = useMemo(() => {
+    return mapSide[side];
+  }, [side]);
+
+  const mountArrow = useCallback(
+    (node) => {
+      arrowRef.current = node;
+      update();
+    },
+    [update]
+  );
+
+  const mountFloating = useCallback(
+    (node) => {
+      refs.floating.current = node;
+      update();
+      setIsMountedFloating(!!node);
+    },
+    [update]
+  );
+
+  const mountReference = useCallback(
+    (node) => {
+      refs.reference.current = node;
+      update();
+      setIsMountedReference(!!node);
+    },
+    [update]
+  );
+
+  if (disabled) {
+    return children;
+  }
+
+  return (
+    <>
+      {React.cloneElement(
+        children,
+        getReferenceProps({
+          ref: mountReference,
+          ...children.props,
+        })
+      )}
+      <FloatingPortal>
+        {open && (
+          <div
+            {...getFloatingProps({
+              ref: mountFloating,
+              className: classes.tooltipContainer,
+              style: {
+                position: strategy,
+                top: y ?? "",
+                left: x ?? "",
+              },
+            })}
+          >
+            <div className={classes.tooltip}>
+              {header && <div>{header}</div>}
+              {title}
+            </div>
+            {hasArrow && (
+              <div
+                className={classes.arrowContainer}
+                ref={mountArrow}
+                style={{
+                  top: arrowY ?? "",
+                  left: arrowX ?? "",
+                  [swappedSide]: "-6px",
+                }}
+              >
+                <div className={classes.relative}>
+                  <svg
+                    className={classes.arrow}
+                    viewBox="0 0 24 15"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M13.6 1.13333L24 15L0 15L10.4 1.13333C11.2 0.0666662 12.8 0.0666661 13.6 1.13333Z"
+                      fill="currentColor"
+                    />
+                  </svg>
+                  <svg
+                    className={classes.arrowOverlay}
+                    viewBox="0 0 24 15"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M13.6 1.13333L24 15L0 15L10.4 1.13333C11.2 0.0666662 12.8 0.0666661 13.6 1.13333Z"
+                      fill="currentColor"
+                    />
+                  </svg>
+                </div>
+              </div>
+            )}
+          </div>
+        )}
+      </FloatingPortal>
+    </>
   );
 };
 
--- src/Tooltip/styles.ts
+++ src/Tooltip/styles.ts
@@ -1,23 +1,107 @@
-import { makeStyles } from "../theme";
+import { rgba } from "polished";
+
+import { makeStyles, SaleorTheme } from "../theme";
 import { TooltipProps } from "./Tooltip";
 
-const useStyles = makeStyles<TooltipProps, "tooltip">(
+type StyleProps = Pick<TooltipProps, "variant">;
+
+export const getBorderColor =
+  (isDark: boolean, theme: SaleorTheme) => (props: StyleProps) => {
+    if (isDark) {
+      switch (props.variant) {
+        case "success":
+          return theme.palette.success.main;
+        case "warning":
+          return theme.palette.warning.main;
+        case "error":
+          return theme.palette.error.main;
+        default:
+          return theme.palette.common.white;
+      }
+    }
+
+    switch (props.variant) {
+      case "success":
+        return theme.palette.success.light;
+      case "warning":
+        return theme.palette.warning.light;
+      case "error":
+        return theme.palette.error.light;
+      default:
+        return rgba(theme.palette.saleor.main[3], 0.6);
+    }
+  };
+
+const useStyles = makeStyles<
+  StyleProps,
+  | "tooltip"
+  | "tooltipContainer"
+  | "dark"
+  | "header"
+  | "relative"
+  | "arrow"
+  | "arrowOverlay"
+  | "arrowContainer"
+>(
   (theme) => ({
+    dark: {},
+    tooltipContainer: {
+      zIndex: 0,
+    },
     tooltip: {
-      backgroundColor: (props) => {
+      backgroundColor: theme.palette.common.white,
+      padding: theme.spacing(1.5),
+      borderRadius: theme.spacing(1),
+      border: `1px solid ${theme.palette.saleor.main[3]}`,
+      borderWidth: "1px",
+      fontWeight: 500,
+      borderColor: getBorderColor(false, theme),
+
+      color: (props) => {
         switch (props.variant) {
-          case "info":
-            return theme.palette.info.main;
           case "success":
-            return theme.palette.success.main;
+            return theme.palette.success.dark;
           case "warning":
-            return theme.palette.warning.main;
+            return theme.palette.warning.dark;
           case "error":
-            return theme.palette.error.main;
+            return theme.palette.error.dark;
           default:
-            return theme.palette.info.main;
+            return theme.palette.saleor.main[1];
         }
       },
+      "&$dark": {
+        color: theme.palette.common.black,
+        backgroundColor: getBorderColor(true, theme),
+      },
+    },
+    header: {},
+    relative: {
+      position: "relative",
+    },
+    arrow: {
+      position: "absolute",
+      zIndex: -1,
+
+      "& path": {
+        stroke: getBorderColor(false, theme),
+        strokeLinecap: "round",
+      },
+
+      "&$dark": {
+        stroke: getBorderColor(true, theme),
+      },
+    },
+    arrowOverlay: {
+      position: "absolute",
+      height: "7.5px",
+      top: "0.5px",
+      width: "100%",
+      zIndex: 1,
+    },
+    arrowContainer: {
+      width: "24px",
+      color: theme.palette.common.white,
+      position: "absolute",
     },
   }),
   {
