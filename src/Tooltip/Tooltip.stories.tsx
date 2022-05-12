d0e20863053486bf4e70c79ec1b45a7b84b63af3
Jonatan Witoszek <jonatanwitoszek@gmail.com>
2022-05-12 15:11:03 +0200

    Update Tooltip stories for Chromatic and testing locally

--- src/Tooltip/Tooltip.stories.tsx
+++ src/Tooltip/Tooltip.stories.tsx
@@ -1,11 +1,15 @@
 import { Meta, Story } from "@storybook/react";
+import isChromatic from "chromatic/isChromatic";
 import React from "react";
 
 import { SquareButton } from "../SquareButton";
+import { makeStyles } from "../theme";
 import { Decorator } from "../utils/Decorator";
 import { Tooltip, TooltipProps } from "./Tooltip";
 
-const Template: Story<TooltipProps> = (args) => <Tooltip {...args} />;
+const Template: Story<TooltipProps> = (args) => {
+  return <Tooltip {...args} />;
+};
 
 export const Default = Template.bind({});
 Default.args = {
@@ -33,29 +37,46 @@ Warning.args = {
   variant: "warning",
 };
 
+const useStyles = makeStyles({
+  pageWrapper: {
+    display: "flex",
+    width: "210vw",
+    height: "150vh",
+  },
+  storyWrapper: {
+    paddingTop: "10%",
+    paddingLeft: "90vw",
+  },
+  tip: {
+    position: "static",
+    left: "12px",
+    top: "12px",
+  },
+});
+
+const TooltipDecorator = (storyFn: any) => {
+  const classes = useStyles();
+
+  if (isChromatic()) {
+    return storyFn();
+  }
+
+  return (
+    <div className={classes.pageWrapper}>
+      <div className={classes.tip}>Scroll to right to test tooltip</div>
+      <div className={classes.storyWrapper}>{storyFn()}</div>
+    </div>
+  );
+};
+
 export default {
   title: "Tooltip",
   component: Tooltip,
   args: {
     title: "Tooltip",
-    open: true,
     children: <SquareButton>Hover</SquareButton>,
     placement: "top-start",
+    open: isChromatic() ? true : undefined,
   },
-  decorators: [
-    (Story) => (
-      <div
-        style={{
-          display: "flex",
-          width: "125vw",
-          height: "150vh",
-        }}
-      >
-        <div style={{ paddingTop: "25%" }}>
-          <Story />
-        </div>
-      </div>
-    ),
-    Decorator,
-  ],
+  decorators: [TooltipDecorator, Decorator],
 } as Meta<TooltipProps>;
