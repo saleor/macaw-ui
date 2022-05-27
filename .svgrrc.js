cbcd5cf78b2ab81a1f3635285960db251299dd24
Jonatan Witoszek <jonatanwitoszek@gmail.com>
2022-05-27 11:41:37 +0200

    Change svg generation to use <SvgIcon> component from Material

--- .svgrrc.js
+++ .svgrrc.js
@@ -1,16 +1,93 @@
 const path = require("path");
 
+function changeSvgToMaterial(jsx) {
+  // replace svg -> SvgIcon
+  jsx.openingElement.name.name = "SvgIcon";
+  jsx.closingElement.name.name = "SvgIcon";
+
+  // adds classes={{ root: classes.root }}
+  jsx.openingElement.attributes.push({
+    type: "JSXAttribute",
+    name: { type: "JSXIdentifier", name: "classes" },
+    value: {
+      type: "JSXExpressionContainer",
+      expression: {
+        type: "ObjectExpression",
+        properties: [
+          {
+            type: "ObjectProperty",
+            method: false,
+            computed: false,
+            shorthand: false,
+            key: {
+              type: "Identifier",
+              name: "root",
+            },
+            value: {
+              type: "MemberExpression",
+              computed: false,
+              optional: false,
+              object: {
+                type: "Identifier",
+                name: "classes",
+              },
+              property: {
+                type: "Identifier",
+                name: "root",
+              },
+            },
+          },
+        ],
+      },
+    },
+  });
+
+  // adds {...props}
+  jsx.openingElement.attributes.push({
+    type: "JSXSpreadAttribute",
+    argument: {
+      type: "Identifier",
+      name: "props",
+    },
+  });
+
+  // adds ref={ref}
+  jsx.openingElement.attributes.push({
+    type: "JSXAttribute",
+    name: {
+      type: "JSXIdentifier",
+      name: "ref",
+    },
+    value: {
+      type: "JSXExpressionContainer",
+      expression: {
+        type: "Identifier",
+        name: "ref",
+      },
+    },
+  });
+  return jsx;
+}
+
 const template = (variables, { tpl }) => {
   const componentName = variables.componentName.replace("Svg", "");
+  changeSvgToMaterial(variables.jsx);
   return tpl`
-import { createSvgIcon } from "@material-ui/core/utils";
-${variables.imports};
+import * as React from "react";
+import SvgIcon from "@material-ui/core/SvgIcon";
+import { useIconStyles } from "../styles";
+import { IconProps } from "../types";
 
 ${variables.interfaces};
 
-export const ${componentName} = createSvgIcon(
-    ${variables.jsx},
-    "${componentName}"
+export const ${componentName} = React.forwardRef(
+  (props: IconProps, ref: React.Ref<SVGSVGElement>) => {
+    const classes = useIconStyles();
+
+    return (
+      ${variables.jsx}
+    )
+  }
 );`;
 };
 
@@ -23,9 +100,10 @@ function indexTemplate(filePaths) {
 }
 
 module.exports = {
-  expandProps: false,
+  expandProps: "end",
   typescript: true,
   svgo: false,
+  ref: true,
   template,
   indexTemplate,
   prettier: false,
