5cd1a6037a31d370fad5f53e3e2f23c8065c32aa
Wojciech <wojciech.mista@hotmail.com>
2022-08-22 11:54:44 +0200

    Fix link color

--- src/theme/Baseline.tsx
+++ src/theme/Baseline.tsx
@@ -59,7 +59,7 @@ const useStyles = makeStyles(
         fontSize: "62.5%",
       },
       a: {
-        color: theme.palette.primary.main,
+        color: theme.palette.saleor.active[1],
         textDecoration: "none",
       },
     },
