7dddb1a12a5d9985ddee2e6206b303d2dc33b1a4
Wojciech <wojciech.mista@hotmail.com>
2022-11-02 13:59:11 +0100

    Fix button border

--- src/theme/createSaleorTheme/overrides/buttons.ts
+++ src/theme/createSaleorTheme/overrides/buttons.ts
@@ -70,6 +70,8 @@ export const buttonOverrides = (
           backgroundColor: colors.main[1],
           borderColor: colors.main[1],
         }),
+
+        borderColor: colors.main[1],
       },
       containedSecondary: {
         ...(isDarkMode && {
