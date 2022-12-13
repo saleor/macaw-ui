cf0052c4856306807b42c86ee550b952f4fffbe9
Paweł Chyła <pawel.chyla@saleor.io>
2022-12-13 15:16:07 +0100

    Add epxand-icon test id for ChevroneIcon

--- src/icons/24x24/ChevronIcon.tsx
+++ src/icons/24x24/ChevronIcon.tsx
@@ -13,6 +13,7 @@ export const ChevronIcon = React.forwardRef(
         viewBox="0 0 24 24"
         fill="none"
         xmlns="http://www.w3.org/2000/svg"
+        data-testId="expand-icon"
         classes={{
           root: classes.root,
         }}
