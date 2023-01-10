a6cb40fc98fb6711c34fa1e0fef69eabc0f84953
Krzysztof Żuraw <9116238+krzysztofzuraw@users.noreply.github.com>
2023-01-10 11:02:38 +0100

    feat: add storybook

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
-import react from "@vitejs/plugin-react-swc";
+import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin";
+import react from "@vitejs/plugin-react";
 import dts from "vite-plugin-dts";
 
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [react(), dts()],
+  plugins: [react(), vanillaExtractPlugin(), dts()],
   build: {
     lib: {
       entry: "src/index.ts",
