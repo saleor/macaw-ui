1cf12a3f1d2155c3ffbf9522ae9f70baa130f602
Krzysztof Żuraw <9116238+krzysztofzuraw@users.noreply.github.com>
2023-01-12 15:16:46 +0100

    Add CSS reset (#203)
    Fixes https://github.com/saleor/macaw-ui/issues/198

--- src/theme/provider.tsx
+++ src/theme/provider.tsx
@@ -1,9 +1,15 @@
 import { defaultLightTheme } from "./defaultLight.css";
 
+import "./reset.css";
+
 type ThemeProviderProps = {
   children: React.ReactNode;
 };
 
 export const ThemeProvider = ({ children }: ThemeProviderProps) => {
-  return <main className={defaultLightTheme}>{children}</main>;
+  return (
+    <main className={defaultLightTheme} id="macaw-ui-root">
+      {children}
+    </main>
+  );
 };
---
+++ src/theme/reset.css.ts
@@ -0,0 +1,30 @@
+import { globalStyle } from "@vanilla-extract/css";
+
+globalStyle("*, *::before, *::after", {
+  boxSizing: "border-box",
+});
+
+globalStyle("*", {
+  margin: 0,
+});
+
+globalStyle("html, body", {
+  height: "100%",
+});
+
+globalStyle("input, button, textarea, select", {
+  fontFamily: "inherit",
+});
+
+globalStyle("img, picture, video, canvas, svg", {
+  display: "block",
+  maxWidth: "100%",
+});
+
+globalStyle("p, h1, h2, h3, h4, h5, h6", {
+  overflowWrap: "break-word",
+});
+
+globalStyle("#macaw-ui-root", {
+  isolation: "isolate",
+});
