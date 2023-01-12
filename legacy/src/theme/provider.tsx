0e4891f1b7039eeec6e01a197971d0b3d300cc7b
Krzysztof Żuraw <9116238+krzysztofzuraw@users.noreply.github.com>
2023-01-12 14:57:09 +0100

    fix: remove leftovers

--- legacy/src/theme/provider.tsx
+++
@@ -1,9 +0,0 @@
-import { defaultLightTheme } from "./defaultLight.css";
-
-type ThemeProviderProps = {
-  children: React.ReactNode;
-};
-
-export const ThemeProvider = ({ children }: ThemeProviderProps) => {
-  return <main className={defaultLightTheme}>{children}</main>;
-};
