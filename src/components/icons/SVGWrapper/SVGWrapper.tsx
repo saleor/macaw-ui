ef76530b588c65b4b57839ad36a2c83e7809f8e4
Paweł Chyła <pawel.chyla@saleor.io>
2023-01-13 14:18:47 +0100

    Icon color from sprinkles

--- src/components/icons/SVGWrapper/SVGWrapper.tsx
+++ src/components/icons/SVGWrapper/SVGWrapper.tsx
@@ -1,25 +1,25 @@
 import { ReactNode, forwardRef } from "react";
+import { Sprinkles, vars } from "~/theme";
 import { svgWrapper, SCVWrapperVaraints } from "./SVGWrapper.css";
 
 export type SVGWrapperProps = SCVWrapperVaraints & {
   className?: string;
   viewBox?: string;
-  color?: string;
   children: ReactNode;
+  color?: Sprinkles["color"];
 };
 
 export const SVGWrapper = forwardRef<SVGSVGElement, SVGWrapperProps>(
-  (
-    { className, size, viewBox = "0 0 24 24", color = "black", children },
-    ref
-  ) => {
+  ({ className, size, viewBox = "0 0 24 24", color, children }, ref) => {
     return (
       <svg
         ref={ref}
         className={[svgWrapper({ size }), className].join(" ")}
         viewBox={viewBox}
         style={{
-          color,
+          color: color
+            ? vars.colors.foreground[color as keyof Sprinkles["color"]]
+            : "currentColor",
         }}
         fill="none"
         xmlns="http://www.w3.org/2000/svg"
