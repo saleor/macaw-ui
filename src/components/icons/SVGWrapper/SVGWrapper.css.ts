1d16ad1c8cdc4cbf39fdbd5a1675b4ba6e6cdfc0
Paweł Chyła <pawel.chyla@saleor.io>
2023-01-13 14:28:00 +0100

    Improve icon sizes

--- src/components/icons/SVGWrapper/SVGWrapper.css.ts
+++ src/components/icons/SVGWrapper/SVGWrapper.css.ts
@@ -6,16 +6,16 @@ export const svgWrapper = recipe({
   variants: {
     size: {
       small: {
-        width: vars.space[6],
-        height: vars.space[6],
+        width: vars.space[7],
+        height: vars.space[7],
       },
       medium: {
         width: vars.space[8],
         height: vars.space[8],
       },
       large: {
-        width: vars.space[10],
-        height: vars.space[10],
+        width: vars.space[9],
+        height: vars.space[9],
       },
     },
   },
