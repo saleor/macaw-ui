f3d28d65996b36cb4a350bd404be1d039e6b23d9
Paweł Chyła <pawel.chyla@saleor.io>
2023-01-13 14:18:14 +0100

    Fix current color in StorefrontIcon

--- src/components/icons/StorefrontIcon.tsx
+++ src/components/icons/StorefrontIcon.tsx
@@ -2,9 +2,9 @@ import { createSVGWrapper } from "./SVGWrapper";
 
 export const StorefrontIcon = createSVGWrapper(
   <path
-    clipRule="currentColor"
+    clipRule="evenodd"
     d="m4 5a1 1 0 0 0 -1 1v12a1 1 0 0 0 1 1h16a1 1 0 0 0 1-1v-12a1 1 0 0 0 -1-1zm2 4a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm13-2h-10v2h10zm-7 4 4 6h-8z"
-    fill="#000"
+    fill="currentColor"
     fillRule="evenodd"
   />
 );
