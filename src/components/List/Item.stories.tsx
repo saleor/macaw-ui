9c934cfbd855623b71ccf27493b24b273a5aee7a
Krzysztof Żuraw <9116238+krzysztofzuraw@users.noreply.github.com>
2023-01-17 13:04:34 +0100

    feat:add basic list item

---
+++ src/components/List/Item.stories.tsx
@@ -0,0 +1,26 @@
+import { ComponentMeta, ComponentStory } from "@storybook/react";
+
+import { Text } from "../Text";
+import { HomeIcon } from "../Icons";
+import { List } from ".";
+
+export default {
+  component: List.Item,
+} as ComponentMeta<typeof List.Item>;
+
+const Template: ComponentStory<typeof List.Item> = (args) => (
+  <List>
+    <List.Item {...args}>
+      <HomeIcon size="large" />
+      <Text>List component</Text>
+    </List.Item>
+  </List>
+);
+
+export const Default = Template.bind({});
+Default.args = {};
+
+export const Disabled = Template.bind({});
+Disabled.args = {
+  disabled: true,
+};
