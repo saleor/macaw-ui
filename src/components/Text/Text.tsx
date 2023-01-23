bedefc3ec3d56b73b91e7612d03baa1bc2d2ae1b
Krzysztof Żuraw <9116238+krzysztofzuraw@users.noreply.github.com>
2023-01-23 14:58:48 +0100

    update list

--- src/components/Text/Text.tsx
+++ src/components/Text/Text.tsx
@@ -12,7 +12,6 @@ type TextProps = TextVariants & {
   as?: "h1" | "h2" | "h3" | "h4" | "h5" | "h6" | "p" | "span";
   color?: Sprinkles["color"];
   className?: string;
-  textTransform?: Sprinkles["textTransform"];
 };
 
 export const Text = ({
@@ -21,7 +20,6 @@ export const Text = ({
   variant,
   size,
   color = "textNeutralDefault",
-  textTransform,
   className,
 }: TextProps) => {
   return (
@@ -29,7 +27,6 @@ export const Text = ({
       as={as}
       className={classNames(text({ variant, size }), className)}
       color={color}
-      textTransform={textTransform}
     >
       {children}
     </Box>
