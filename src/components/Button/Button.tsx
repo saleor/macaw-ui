34a3097656750dea65e6be2243cf90512dd278a0
Michał Droń <dron.official@yahoo.com>
2023-01-31 16:29:03 +0100

    Allow passing refs to Button component

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,34 +1,41 @@
-import { ReactNode } from "react";
+import { forwardRef, ReactNode } from "react";
 
 import { classNames } from "~/utils";
 
 import { Box } from "../Box";
 import { button, ButtonVariants } from "./Button.css";
 
-export type ButtonProps = ButtonVariants &
-  Omit<React.ButtonHTMLAttributes<HTMLButtonElement>, "color"> & {
-    children: ReactNode;
-    disabled?: boolean;
-    className?: string;
-  };
-
-export const Button = ({
-  children,
-  size,
-  variant,
-  fixedWidth = false,
-  disabled,
-  className,
-  ...props
-}: ButtonProps) => {
-  return (
-    <Box
-      as="button"
-      className={classNames(button({ variant, size, fixedWidth }), className)}
-      disabled={disabled}
-      {...props}
-    >
-      {children}
-    </Box>
-  );
+export type ButtonProps = ButtonVariants & {
+  children: ReactNode;
+  disabled?: boolean;
+  className?: string;
 };
+
+export const Button = forwardRef<HTMLButtonElement, ButtonProps>(
+  (
+    {
+      children,
+      size,
+      variant,
+      fixedWidth = false,
+      disabled,
+      className,
+      ...props
+    },
+    ref
+  ) => {
+    return (
+      <Box
+        as="button"
+        className={classNames(button({ variant, size, fixedWidth }), className)}
+        disabled={disabled}
+        ref={ref}
+        {...props}
+      >
+        {children}
+      </Box>
+    );
+  }
+);
+
+Button.displayName = "Button";
