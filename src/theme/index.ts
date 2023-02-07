d8f274f7e10f68662ef5c4ac4757b63cda66da07
Krzysztof Żuraw <9116238+krzysztofzuraw@users.noreply.github.com>
2023-02-07 10:51:26 +0100

    feat: export theme colors

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -2,4 +2,5 @@ export * from "./contract.css";
 export * from "./provider";
 export * from "./sprinkles.css";
 export { useTheme } from "./context";
+export { themes } from "./themes";
 export type { DefaultTheme } from "./themes";
