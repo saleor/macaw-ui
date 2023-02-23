c83591155bd675e18d39e00d3003175c0e22ac48
Paweł Chyła <pawel.chyla@saleor.io>
2023-02-23 11:13:40 +0100

    Remove box

--- src/components/List/ItemGroup/Trigger.tsx
+++ src/components/List/ItemGroup/Trigger.tsx
@@ -2,7 +2,7 @@ import { AccordionTrigger } from "@radix-ui/react-accordion";
 import { ReactNode } from "react";
 
 import { Sprinkles } from "~/theme";
-import { Box, Button, ChervonDownIcon } from "~/components";
+import { Button, ChervonDownIcon } from "~/components";
 import { DataAttributes } from "~/components/types";
 
 import { List } from "..";
@@ -18,17 +18,10 @@ type ItemGroupTriggerProps = Sprinkles &
     url?: string;
   };
 
-export const Trigger = ({
-  children,
-  size,
-  url,
-  ...rest
-}: ItemGroupTriggerProps) => {
+export const Trigger = ({ children, size, ...rest }: ItemGroupTriggerProps) => {
   return (
     <List.Item {...rest}>
-      <Box textDecoration="none" as={url ? "a" : "span"} href={url}>
-        {children}
-      </Box>
+      {children}
       <AccordionTrigger asChild>
         <Button
           icon={
