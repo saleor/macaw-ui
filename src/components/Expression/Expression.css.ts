af622a5b1d03c256b87716852f933fc6224002ff
andrzejewsky <vox3r69@gmail.com>
2023-02-28 16:50:49 +0100

    Expression setup

--- src/components/Expression/Expression.css.ts
+++
@@ -1,6 +0,0 @@
-// import { recipe, RecipeVariants } from "@vanilla-extract/recipes";
-// import { sprinkles } from "~/theme";
-
-// export const filter = recipe({});
-
-// export type ButtonVariants = RecipeVariants<typeof button>;
