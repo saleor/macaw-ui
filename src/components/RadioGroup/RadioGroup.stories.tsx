f482bfc59bceaacfb60def860d1c39cb3c1645cd
Krzysztof Żuraw <9116238+krzysztofzuraw@users.noreply.github.com>
2023-03-01 16:12:30 +0100

    wip

---
+++ src/components/RadioGroup/RadioGroup.stories.tsx
@@ -0,0 +1,9 @@
+import { ComponentMeta } from "@storybook/react";
+
+import { RadioGroupItem } from "./RadioGroup";
+
+export default {
+  component: RadioGroupItem,
+} as ComponentMeta<typeof RadioGroupItem>;
+
+export const Default = () => <RadioGroupItem />;
