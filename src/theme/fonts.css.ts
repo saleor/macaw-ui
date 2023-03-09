7745f05dbf8d560355d9df3983987040cc1dc340
Krzysztof Żuraw <9116238+krzysztofzuraw@users.noreply.github.com>
2023-03-09 15:54:12 +0100

    fix: typos in fontc.css.ts

--- src/theme/fonts.css.ts
+++ src/theme/fonts.css.ts
@@ -3,126 +3,126 @@ import { globalFontFace, globalStyle } from "@vanilla-extract/css";
 // adapted from https://rsms.me/inter/inter.css
 
 globalFontFace("Inter", {
-  src: 'url("./Inter-Thin.var.woff2") format("woff2")',
+  src: 'url("./Inter-Thin.woff2") format("woff2")',
   fontDisplay: "swap",
   fontStyle: "normal",
   fontWeight: "100",
 });
 
 globalFontFace("Inter", {
-  src: 'url("./Inter-ThinItalic.var.woff2") format("woff2")',
+  src: 'url("./Inter-ThinItalic.woff2") format("woff2")',
   fontDisplay: "swap",
   fontStyle: "italic",
   fontWeight: "100",
 });
 
 globalFontFace("Inter", {
-  src: 'url("./Inter-ExtraLight.var.woff2") format("woff2")',
+  src: 'url("./Inter-ExtraLight.woff2") format("woff2")',
   fontDisplay: "swap",
   fontStyle: "normal",
   fontWeight: "200",
 });
 
 globalFontFace("Inter", {
-  src: 'url("./Inter-ExtraLightItalic.var.woff2") format("woff2")',
+  src: 'url("./Inter-ExtraLightItalic.woff2") format("woff2")',
   fontDisplay: "swap",
   fontStyle: "italic",
   fontWeight: "200",
 });
 
 globalFontFace("Inter", {
-  src: 'url("./Inter-Light.var.woff2") format("woff2")',
+  src: 'url("./Inter-Light.woff2") format("woff2")',
   fontDisplay: "swap",
   fontStyle: "normal",
   fontWeight: "300",
 });
 
 globalFontFace("Inter", {
-  src: 'url("./Inter-LightItalic.var.woff2") format("woff2")',
+  src: 'url("./Inter-LightItalic.woff2") format("woff2")',
   fontDisplay: "swap",
   fontStyle: "italic",
   fontWeight: "300",
 });
 
 globalFontFace("Inter", {
-  src: 'url("./Inter-Regular.var.woff2") format("woff2")',
+  src: 'url("./Inter-Regular.woff2") format("woff2")',
   fontDisplay: "swap",
   fontStyle: "normal",
   fontWeight: "400",
 });
 
 globalFontFace("Inter", {
-  src: 'url("./Inter-Italic.var.woff2") format("woff2")',
+  src: 'url("./Inter-Italic.woff2") format("woff2")',
   fontDisplay: "swap",
   fontStyle: "italic",
   fontWeight: "400",
 });
 
 globalFontFace("Inter", {
-  src: 'url("./Inter-Medium.var.woff2") format("woff2")',
+  src: 'url("./Inter-Medium.woff2") format("woff2")',
   fontDisplay: "swap",
   fontStyle: "normal",
   fontWeight: "500",
 });
 
 globalFontFace("Inter", {
-  src: 'url("./Inter-MediumItalic.var.woff2") format("woff2")',
+  src: 'url("./Inter-MediumItalic.woff2") format("woff2")',
   fontDisplay: "swap",
   fontStyle: "italic",
   fontWeight: "500",
 });
 
 globalFontFace("Inter", {
-  src: 'url("./Inter-SemiBold.var.woff2") format("woff2")',
+  src: 'url("./Inter-SemiBold.woff2") format("woff2")',
   fontDisplay: "swap",
   fontStyle: "normal",
   fontWeight: "600",
 });
 
 globalFontFace("Inter", {
-  src: 'url("./Inter-SemiBoldItalic.var.woff2") format("woff2")',
+  src: 'url("./Inter-SemiBoldItalic.woff2") format("woff2")',
   fontDisplay: "swap",
   fontStyle: "italic",
   fontWeight: "600",
 });
 
 globalFontFace("Inter", {
-  src: 'url("./Inter-Bold.var.woff2") format("woff2")',
+  src: 'url("./Inter-Bold.woff2") format("woff2")',
   fontDisplay: "swap",
   fontStyle: "normal",
   fontWeight: "700",
 });
 
 globalFontFace("Inter", {
-  src: 'url("./Inter-BoldItalic.var.woff2") format("woff2")',
+  src: 'url("./Inter-BoldItalic.woff2") format("woff2")',
   fontDisplay: "swap",
   fontStyle: "italic",
   fontWeight: "700",
 });
 
 globalFontFace("Inter", {
-  src: 'url("./Inter-ExtraBold.var.woff2") format("woff2")',
+  src: 'url("./Inter-ExtraBold.woff2") format("woff2")',
   fontDisplay: "swap",
   fontStyle: "normal",
   fontWeight: "800",
 });
 
 globalFontFace("Inter", {
-  src: 'url("./Inter-ExtraBoldItalic.var.woff2") format("woff2")',
+  src: 'url("./Inter-ExtraBoldItalic.woff2") format("woff2")',
   fontDisplay: "swap",
   fontStyle: "italic",
   fontWeight: "800",
 });
 
 globalFontFace("Inter", {
-  src: 'url("./Inter-Black.var.woff2") format("woff2")',
+  src: 'url("./Inter-Black.woff2") format("woff2")',
   fontDisplay: "swap",
   fontStyle: "normal",
   fontWeight: "900",
 });
 
 globalFontFace("Inter", {
-  src: 'url("./Inter-ExtraBlackItalic.var.woff2") format("woff2")',
+  src: 'url("./Inter-BlackItalic.woff2") format("woff2")',
   fontDisplay: "swap",
   fontStyle: "italic",
   fontWeight: "900",
