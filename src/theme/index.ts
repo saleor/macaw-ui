42857ac8bd6fa54b438e287fc9a6f76409ae4329
Krzysztof Żuraw <9116238+krzysztofzuraw@users.noreply.github.com>
2023-03-13 11:01:48 +0100

    fix: missing exported type (#296)

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -2,6 +2,5 @@ export * from "./contract.css";
 export * from "./provider";
 export * from "./sprinkles.css";
 export { useTheme } from "./context";
-export { themes } from "./themes";
-export type { DefaultTheme } from "./themes";
+export * from "./themes";
 export * from "./media-queries";
