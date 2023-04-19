3218c4a700f1994c2c0589b2fd3c6e5827509873
Patryk Andrzejewski <vox3r69@gmail.com>
2023-04-19 12:10:41 +0200

    docs: new stories for text, toogle, tooltip (#381)
    * Docs for text, toogle, tooltip
    
    * Remove unused components

--- src/components/Text/Text.stories.tsx
+++ src/components/Text/Text.stories.tsx
@@ -1,129 +1,67 @@
-import { ComponentMeta } from "@storybook/react";
+import { Meta, StoryObj } from "@storybook/react";
+import { Text } from "./index";
 
-import { Text } from "./Text";
-import { Box } from "../Box";
-
-export default {
+const meta: Meta<typeof Text> = {
+  title: "Components / Text",
+  tags: ["autodocs"],
   component: Text,
-} as ComponentMeta<typeof Text>;
+};
 
-export const Default = () => {
-  return (
-    <Box display="flex" flexWrap="wrap" gap={5}>
-      <Box display="flex" flexDirection="column" gap={5}>
-        <Box as="pre" paddingBottom={2} borderBottomStyle="solid">
-          Small
-        </Box>
-        <Text variant="hero" size="small">
-          Best eCommerce Platform
-        </Text>
-        <Text variant="title" size="small">
-          Dashboard
-        </Text>
-        <Text variant="heading" size="small">
-          Product Details
-        </Text>
-        <Text variant="bodyStrong" size="small">
-          Sapien parturient in fringilla.
-        </Text>
+export default meta;
+type Story = StoryObj<typeof Text>;
 
-        <Text variant="bodyEmp" size="small">
-          Sapien parturient in fringilla.
-        </Text>
+export const Primary: Story = {
+  args: {
+    variant: "hero",
+    children: "Some text",
+  },
+};
 
-        <Text variant="body" size="small">
-          Sapien parturient in fringilla ac facilisis nunc adipiscing viverra.
-          Non semper rutrum augue sed leo felis pretium sed.
-        </Text>
+export const Title: Story = {
+  args: {
+    ...Primary.args,
+    variant: "title",
+  },
+};
 
-        <Text variant="button" size="small">
-          Edit element
-        </Text>
+export const Heading: Story = {
+  args: {
+    ...Primary.args,
+    variant: "heading",
+  },
+};
 
-        <Text variant="caption" size="small">
-          Name and label
-        </Text>
-      </Box>
-      <Text variant="hero" size="medium">
-        Best eCommerce Platform
-      </Text>
-      <Text>Dashboard</Text>
-      <Text variant="heading" size="medium">
-        Product Details
-      </Text>
-      <Text variant="bodyStrong" size="medium">
-        Sapien parturient in fringilla.
-      </Text>
+export const BodyStrong: Story = {
+  args: {
+    ...Primary.args,
+    variant: "bodyStrong",
+  },
+};
 
-      <Box display="flex" flexDirection="column" gap={5}>
-        <Box as="pre" paddingBottom={2} borderBottomStyle="solid">
-          Medium
-        </Box>
-        <Text variant="hero" size="medium">
-          Best eCommerce Platform
-        </Text>
-        <Text variant="title" size="medium">
-          Dashboard
-        </Text>
-        <Text variant="heading" size="medium">
-          Product Details
-        </Text>
-        <Text variant="bodyStrong" size="medium">
-          Sapien parturient in fringilla.
-        </Text>
+export const BodyEmp: Story = {
+  args: {
+    ...Primary.args,
+    variant: "bodyEmp",
+  },
+};
 
-        <Text variant="bodyEmp" size="medium">
-          Sapien parturient in fringilla.
-        </Text>
+export const Body: Story = {
+  args: {
+    ...Primary.args,
+    variant: "body",
+  },
+};
 
-        <Text variant="body" size="medium">
-          Sapien parturient in fringilla ac facilisis nunc adipiscing viverra.
-          Non semper rutrum augue sed leo felis pretium sed.
-        </Text>
+export const Button: Story = {
+  args: {
+    ...Primary.args,
+    variant: "button",
+  },
+};
 
-        <Text variant="button" size="medium">
-          Edit element
-        </Text>
-
-        <Text variant="caption" size="medium">
-          Name and label
-        </Text>
-      </Box>
-
-      <Box display="flex" flexDirection="column" gap={5}>
-        <Box as="pre" paddingBottom={2} borderBottomStyle="solid">
-          Large
-        </Box>
-        <Text variant="hero" size="large">
-          Best eCommerce Platform
-        </Text>
-        <Text variant="title" size="large">
-          Dashboard
-        </Text>
-        <Text variant="heading" size="large">
-          Product Details
-        </Text>
-        <Text variant="bodyStrong" size="large">
-          Sapien parturient in fringilla.
-        </Text>
-
-        <Text variant="bodyEmp" size="large">
-          Sapien parturient in fringilla.
-        </Text>
-
-        <Text variant="body" size="large">
-          Sapien parturient in fringilla ac facilisis nunc adipiscing viverra.
-          Non semper rutrum augue sed leo felis pretium sed.
-        </Text>
-
-        <Text variant="button" size="large">
-          Edit element
-        </Text>
-
-        <Text variant="caption" size="large">
-          Name and label
-        </Text>
-      </Box>
-    </Box>
-  );
+export const Caption: Story = {
+  args: {
+    ...Primary.args,
+    variant: "caption",
+  },
 };
