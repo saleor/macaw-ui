ccb1782d617c87e511b30692a2e952f05642514b
Patryk Andrzejewski <vox3r69@gmail.com>
2023-04-20 13:46:38 +0200

    docs: documenting token values (#383)
    * Docs for tokens
    
    * Fix typos

--- .storybook/components/Spacings.tsx
+++ .storybook/components/Spacings.tsx
@@ -1,22 +1,27 @@
-import { Box } from "~/components";
+import { Box, Text } from "~/components";
 import { useTheme } from "~/theme";
+import { Unstyled } from "@storybook/blocks";
 
 export const SpacingsPresentation = () => {
   const { themeValues: vars } = useTheme();
 
   return (
-    <Box display="flex" flexDirection="row" flexWrap="wrap" rowGap={10}>
-      {Object.entries(vars.space).map(([key, value]) => {
-        return (
-          <Box width="100%" key={key}>
-            <Box>space-{key}</Box>
-            <Box>{value}</Box>
-            <Box key={key} __backgroundColor="#e3eaf2" position="relative">
-              <Box __width={value} height={10} __backgroundColor="#056dff" />
+    <Unstyled>
+      <Box display="flex" flexDirection="row" flexWrap="wrap" rowGap={10}>
+        {Object.entries(vars.space).map(([key, value]) => {
+          return (
+            <Box width="100%" key={key}>
+              <Box>space-{key}</Box>
+              <Text variant="caption" __minWidth="100px">
+                {value}
+              </Text>
+              <Box key={key} __backgroundColor="#e3eaf2" position="relative">
+                <Box __width={value} height={10} __backgroundColor="#056dff" />
+              </Box>
             </Box>
-          </Box>
-        );
-      })}
-    </Box>
+          );
+        })}
+      </Box>
+    </Unstyled>
   );
 };
