437223b674ccb92d211c441797439008acd3353a
Michał Droń <droniu@droniu.dev>
2023-04-27 10:34:55 +0200

    fix: render popover content as child (#404)
    * Remove padding
    
    * Render popover content as child

--- src/components/Popover/Content.tsx
+++ src/components/Popover/Content.tsx
@@ -23,6 +23,7 @@ export const Content = ({
   return (
     <RadixPopoverPortal>
       <RadixPopoverContent
+        asChild
         className={classNames(popover, className)}
         {...props}
       >
