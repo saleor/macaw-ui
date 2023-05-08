9486749e7b0c7372243371208e9a57e4834d09e0
Paweł Chyła <chyla1988@gmail.com>
2023-05-08 22:05:40 +0200

    feat: add ref to tooltip trigger (#420)
    * Add ref to tooltip trigger
    
    * Remove on focus

--- src/components/Tooltip/Trigger.tsx
+++ src/components/Tooltip/Trigger.tsx
@@ -1,10 +1,18 @@
-import { ReactNode } from "react";
+import { ReactNode, forwardRef } from "react";
 import { Trigger as RadixTooltipTrigger } from "@radix-ui/react-tooltip";
 
 export interface TooltipTriggerProps {
   children: ReactNode;
 }
 
-export const Trigger = ({ children }: TooltipTriggerProps) => {
-  return <RadixTooltipTrigger asChild>{children}</RadixTooltipTrigger>;
-};
+export const Trigger = forwardRef<HTMLButtonElement, TooltipTriggerProps>(
+  ({ children }, ref) => {
+    return (
+      <RadixTooltipTrigger asChild ref={ref}>
+        {children}
+      </RadixTooltipTrigger>
+    );
+  }
+);
+
+Trigger.displayName = "TooltipTrigger";
