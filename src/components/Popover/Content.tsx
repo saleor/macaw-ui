ce0c06c0bdb9a52a86feec9e939161243fe3329d
Michał Droń <droniu@droniu.dev>
2023-05-15 13:37:26 +0200

    Expose collision padding in Popover Content

--- src/components/Popover/Content.tsx
+++ src/components/Popover/Content.tsx
@@ -12,6 +12,7 @@ export interface PopoverContentProps {
   align?: "start" | "center" | "end";
   alignOffset?: number;
   avoidCollisions?: boolean;
+  collisionPadding?: number;
   children: React.ReactNode;
 }
 
