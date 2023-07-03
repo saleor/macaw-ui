7b82ad8694614ff5412c4bf04f0af264c5065b8b
Krzysztof Żuraw <9116238+krzysztofzuraw@users.noreply.github.com>
2023-07-03 17:36:22 +0200

    fix: proper types

--- src/components/ExperimentalFilters/useEvents.tsx
+++ src/components/ExperimentalFilters/useEvents.tsx
@@ -2,30 +2,68 @@ import { useEffect } from "react";
 import { FilterEventEmitter } from "./EventEmitter";
 
 export interface FilterEvent extends Event {
-  detail?: {
-    type:
-      | "leftOperator.onChange"
-      | "leftOperator.onInputValueChange"
-      | "leftOperator.onFocus"
-      | "leftOperator.onBlur"
-      | "condition.onChange"
-      | "condition.onScrollEnd"
-      | "condition.onFocus"
-      | "condition.onBlur"
-      | "rightOperator.onChange"
-      | "rightOperator.onInputValueChange"
-      | "rightOperator.onScrollEnd"
-      | "rightOperator.onFocus"
-      | "rightOperator.onBlur"
-      | "row.remove"
-      | "row.add";
-    value?:
-      | string
-      | { value: string; label: string }[]
-      | { label: string; value: string };
-    path?: string;
-    rowType?: string;
-  };
+  detail?:
+    | {
+        type: "row.add";
+        rowType: string;
+      }
+    | {
+        type: "row.remove";
+        path: string;
+      }
+    | {
+        type: "leftOperator.onChange";
+        path: string;
+        value: { label: string; value: string; type: string };
+        rowType: string;
+      }
+    | {
+        type: "leftOperator.onFocus";
+        path: string;
+      }
+    | {
+        type: "leftOperator.onBlur";
+        path: string;
+      }
+    | {
+        type: "leftOperator.onInputValueChange";
+        path: string;
+        value: string;
+      }
+    | {
+        type: "condition.onChange";
+        path: string;
+        value: { label: string; value: string; type: string };
+      }
+    | {
+        type: "condition.onFocus";
+        path: string;
+      }
+    | {
+        type: "condition.onBlur";
+        path: string;
+      }
+    | {
+        type: "rightOperator.onChange";
+        path: string;
+        value:
+          | string
+          | { label: string; value: string; type: string }[]
+          | { label: string; value: string; type: string };
+      }
+    | {
+        type: "rightOperator.onFocus";
+        path: string;
+      }
+    | {
+        type: "rightOperator.onBlur";
+        path: string;
+      }
+    | {
+        type: "rightOperator.onInputValueChange";
+        path: string;
+        value: string;
+      };
 }
 
 type UseEventsProps = {
