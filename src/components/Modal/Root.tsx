4cb7fbe268ce76bab7964bff1c41f2d67adb9ab8
Krzysztof Żuraw <9116238+krzysztofzuraw@users.noreply.github.com>
2023-08-16 13:00:37 +0200

    feat: controlled modal

--- src/components/Modal/Root.tsx
+++ src/components/Modal/Root.tsx
@@ -2,10 +2,20 @@ import { Root as DialogRoot } from "@radix-ui/react-dialog";
 
 export type ModalRootProps = {
   children: React.ReactNode;
+  open?: boolean;
+  onChange?: (open: boolean) => void;
 };
 
-export const Root = ({ children }: ModalRootProps) => {
-  return <DialogRoot data-macaw-ui-component="Modal">{children}</DialogRoot>;
+export const Root = ({ children, open, onChange }: ModalRootProps) => {
+  return (
+    <DialogRoot
+      data-macaw-ui-component="Modal"
+      open={open}
+      onOpenChange={onChange}
+    >
+      {children}
+    </DialogRoot>
+  );
 };
 
 Root.displayName = "Modal";
