be3d92f7495588db8741960cbcc3f57ff35141d4
Krzysztof Żuraw <9116238+krzysztofzuraw@users.noreply.github.com>
2023-08-17 10:38:20 +0200

    feat: controlled modal (#537)

--- src/components/Modal/Modal.stories.tsx
+++ src/components/Modal/Modal.stories.tsx
@@ -1,5 +1,6 @@
 import { Meta, StoryObj } from "@storybook/react";
 
+import { useState } from "react";
 import { Box, Button, CloseIcon, Text } from "..";
 
 import { Modal } from ".";
@@ -46,4 +47,39 @@ export const Primary: Story = {
       </Modal.Content>,
     ],
   },
+};
+
+export const Controlled = () => {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <>
+      <Button onClick={() => setOpen(true)}>Show modal</Button>
+      <Modal open={open} onChange={setOpen}>
+        <Modal.Content>
+          <Box
+            backgroundColor="surfaceNeutralPlain"
+            boxShadow="modal"
+            __left="50%"
+            __top="50%"
+            position="fixed"
+            __maxWidth="400px"
+            __transform="translate(-50%, -50%)"
+          >
+            <Box
+              display="flex"
+              gap={3}
+              justifyContent="center"
+              alignItems="center"
+            >
+              <Text>Modal content!</Text>
+              <Modal.Close>
+                <Button variant="tertiary" icon={<CloseIcon />} size="small" />
+              </Modal.Close>
+            </Box>
+          </Box>
+        </Modal.Content>
+      </Modal>
+    </>
+  );
 };
