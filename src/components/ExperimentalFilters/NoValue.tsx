131e1fb1595d94014d7f11efd86ee59d87d75ae5
Krzysztof Żuraw <9116238+krzysztofzuraw@users.noreply.github.com>
2023-10-05 15:42:42 +0200

    feat: move experimental filters out of library

--- src/components/ExperimentalFilters/NoValue.tsx
+++
@@ -1,9 +0,0 @@
-import { Box, Text } from "..";
-
-export const NoValue = ({ locale }: { locale: Record<string, string> }) => {
-  return (
-    <Box display="flex" justifyContent="center" alignItems="center">
-      <Text>{locale.noValueText}</Text>
-    </Box>
-  );
-};
