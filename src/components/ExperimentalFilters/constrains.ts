92383bdaa86dd7c4dfc0ee5148076a6f068585cb
Krzysztof Żuraw <9116238+krzysztofzuraw@users.noreply.github.com>
2023-10-06 08:16:23 +0200

    feat: move experimental filters out of library (#610)

--- src/components/ExperimentalFilters/constrains.ts
+++
@@ -1,8 +0,0 @@
-import { Row } from "./types";
-
-export const getItemConstraint = (constraint: Row["constraint"]) => ({
-  disableRemoveButton: constraint?.removable === false ? true : false,
-  disableLeftOperator: constraint?.disabled?.includes("left") ?? false,
-  disableCondition: constraint?.disabled?.includes("condition") ?? false,
-  disableRightOperator: constraint?.disabled?.includes("right") ?? false,
-});
