1efe97835344455dc73478d2e6518259b0e9b1d6
Paweł Chyła <pawel.chyla@saleor.io>
2023-10-19 18:40:23 +0200

    Allow to scroll insie  modal content

--- src/components/Modal/Modal.stories.tsx
+++ src/components/Modal/Modal.stories.tsx
@@ -1,7 +1,7 @@
 import { Meta, StoryObj } from "@storybook/react";
 
 import { useState } from "react";
-import { Box, Button, CloseIcon, Text } from "..";
+import { Box, Button, CloseIcon, Combobox, Text } from "..";
 
 import { Modal } from ".";
 
@@ -73,6 +73,56 @@ export const Controlled = () => {
               alignItems="center"
             >
               <Text>Modal content!</Text>
+              <Modal.Close>
+                <Button variant="tertiary" icon={<CloseIcon />} size="small" />
+              </Modal.Close>
+            </Box>
+          </Box>
+        </Modal.Content>
+      </Modal>
+    </>
+  );
+};
+
+export const WithComboboxInside = () => {
+  const [open, setOpen] = useState(false);
+  const [value, setValue] = useState<string | null>("color-black");
+
+  return (
+    <>
+      <Button onClick={() => setOpen(true)}>Show modal</Button>
+      <Modal open={open} onChange={setOpen}>
+        <Modal.Content>
+          <Box
+            backgroundColor="surfaceNeutralPlain"
+            boxShadow="modal"
+            __left="50%"
+            __top="50%"
+            position="fixed"
+            __maxWidth="400px"
+            __transform="translate(-50%, -50%)"
+          >
+            <Box
+              display="flex"
+              gap={3}
+              padding={10}
+              justifyContent="center"
+              alignItems="center"
+            >
+              <Combobox
+                label="Pick a color"
+                size="large"
+                value={value}
+                onChange={(value) => setValue(value)}
+                options={[
+                  { value: "color-black", label: "Black" },
+                  { value: "color-red", label: "Red" },
+                  { value: "color-green", label: "Green" },
+                  { value: "color-blue", label: "Blue" },
+                  { value: "color-orange", label: "Orange" },
+                  { value: "color-purple", label: "Purple" },
+                ]}
+              />
               <Modal.Close>
                 <Button variant="tertiary" icon={<CloseIcon />} size="small" />
               </Modal.Close>
