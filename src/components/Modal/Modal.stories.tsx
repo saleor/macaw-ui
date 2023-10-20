e56cc9faa2b168ac69080e89450b3df542d7bd8d
Paweł Chyła <pawel.chyla@saleor.io>
2023-10-20 09:59:43 +0200

    Fix stories

--- src/components/Modal/Modal.stories.tsx
+++ src/components/Modal/Modal.stories.tsx
@@ -84,50 +84,49 @@ export const Controlled = () => {
   );
 };
 
-export const WithComboboxInside = () => {
-  const [open, setOpen] = useState(false);
-
-  return (
-    <>
-      <Button onClick={() => setOpen(true)}>Show modal</Button>
-      <Modal open={open} onChange={setOpen}>
-        <Modal.Content>
+export const WithComboboxInside: Story = {
+  args: {
+    children: [
+      // eslint-disable-next-line react/jsx-key
+      <Modal.Trigger>
+        <Button variant="tertiary">Show modal</Button>
+      </Modal.Trigger>,
+      // eslint-disable-next-line react/jsx-key
+      <Modal.Content>
+        <Box
+          backgroundColor="surfaceNeutralPlain"
+          boxShadow="modal"
+          __left="50%"
+          __top="50%"
+          position="fixed"
+          __maxWidth="400px"
+          __transform="translate(-50%, -50%)"
+        >
           <Box
-            backgroundColor="surfaceNeutralPlain"
-            boxShadow="modal"
-            __left="50%"
-            __top="50%"
-            position="fixed"
-            __maxWidth="400px"
-            __transform="translate(-50%, -50%)"
+            display="flex"
+            gap={3}
+            justifyContent="center"
+            alignItems="center"
           >
-            <Box
-              display="flex"
-              gap={3}
-              padding={10}
-              justifyContent="center"
-              alignItems="center"
-            >
-              <Combobox
-                label="Pick a color"
-                size="large"
-                value="color-black"
-                options={[
-                  { value: "color-black", label: "Black" },
-                  { value: "color-red", label: "Red" },
-                  { value: "color-green", label: "Green" },
-                  { value: "color-blue", label: "Blue" },
-                  { value: "color-orange", label: "Orange" },
-                  { value: "color-purple", label: "Purple" },
-                ]}
-              />
-              <Modal.Close>
-                <Button variant="tertiary" icon={<CloseIcon />} size="small" />
-              </Modal.Close>
-            </Box>
+            <Combobox
+              label="Pick a color"
+              size="large"
+              value="color-black"
+              options={[
+                { value: "color-black", label: "Black" },
+                { value: "color-red", label: "Red" },
+                { value: "color-green", label: "Green" },
+                { value: "color-blue", label: "Blue" },
+                { value: "color-orange", label: "Orange" },
+                { value: "color-purple", label: "Purple" },
+              ]}
+            />
+            <Modal.Close>
+              <Button variant="tertiary" icon={<CloseIcon />} size="small" />
+            </Modal.Close>
           </Box>
-        </Modal.Content>
-      </Modal>
-    </>
-  );
+        </Box>
+      </Modal.Content>,
+    ],
+  },
 };
