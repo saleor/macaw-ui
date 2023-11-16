9e3d5028856812d925a38e60fafeb7d5d665c6bd
Paweł Chyła <chyla1988@gmail.com>
2023-11-16 12:37:09 +0100

    fix: handling 0 vales in input component (#647)
    * Fix handling 0 vales in input
    
    * Add comment

--- src/components/Input/helpers.ts
+++ src/components/Input/helpers.ts
@@ -1,6 +1,7 @@
 import { KeyboardEvent } from "react";
 
 import { InputProps } from "./Input";
+import { InputValue } from "./types";
 
 // Check if input type number is valid as input type number doesn't currently work in browsers like Safari and Firefox
 export const checkIfValidNumberInput = (event: KeyboardEvent<HTMLElement>) => {
@@ -10,5 +11,22 @@ export const checkIfValidNumberInput = (event: KeyboardEvent<HTMLElement>) => {
   return !event.key.match(allowedCharacter) && event.preventDefault();
 };
 
-export const checkIfDateTimeInput = (type: InputProps["type"]) =>
+export const isInputTyped = (
+  type: InputProps["type"],
+  value: InputValue,
+  active: boolean
+): boolean => {
+  // do not scale label down if input is date, time or datetime-local
+  if (checkIfDateTimeInput(type)) {
+    return true;
+  }
+
+  if (value !== "" && value !== undefined) {
+    return true;
+  }
+
+  return active;
+};
+
+const checkIfDateTimeInput = (type: InputProps["type"]) =>
   ["date", "time", "datetime-local"].includes(type ?? "");
