4784409e24d7e04cc457219647b071497c0e3f65
Paweł Chyła <pawel.chyla@saleor.io>
2024-08-19 23:24:32 +0200

    log higlight index

--- src/components/Combobox/Common/useCombobox.tsx
+++ src/components/Combobox/Common/useCombobox.tsx
@@ -53,6 +53,8 @@ export const useCombobox = <T extends Option, V extends string | Option>({
     selectedItem
   );
 
+  console.log("highlightedIndex", highlightedIndex)
+
   const {
     isOpen,
     getToggleButtonProps,
