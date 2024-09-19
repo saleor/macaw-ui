5fd4f3f18375d788c5b43ade112de43e2baf04b3
Wojciech <wojciech.mista@hotmail.com>
2024-09-19 12:27:41 +0200

    one path only

--- src/components/Icons/HelpIcon.tsx
+++ src/components/Icons/HelpIcon.tsx
@@ -1,48 +1,11 @@
 import { createSVGWrapper } from "./SVGWrapper";
 
 export const HelpIcon = createSVGWrapper(
-  <>
-    <path
-      d="M9.99984 18.3333C14.6022 18.3333 18.3332 14.6024 18.3332 10C18.3332 5.39763 14.6022 1.66667 9.99984 1.66667C5.39746 1.66667 1.6665 5.39763 1.6665 10C1.6665 14.6024 5.39746 18.3333 9.99984 18.3333Z"
-      stroke="black"
-      strokeWidth="1.75"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    />
-    <path
-      d="M4.1084 4.10834L7.64173 7.64167"
-      stroke="black"
-      strokeWidth="1.75"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    />
-    <path
-      d="M12.3584 7.64167L15.8917 4.10834"
-      stroke="black"
-      strokeWidth="1.75"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    />
-    <path
-      d="M12.3584 12.3583L15.8917 15.8917"
-      stroke="black"
-      strokeWidth="1.75"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    />
-    <path
-      d="M7.64173 12.3583L4.1084 15.8917"
-      stroke="black"
-      strokeWidth="1.75"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    />
-    <path
-      d="M9.99984 13.3333C11.8408 13.3333 13.3332 11.841 13.3332 10C13.3332 8.15906 11.8408 6.66667 9.99984 6.66667C8.15889 6.66667 6.6665 8.15906 6.6665 10C6.6665 11.841 8.15889 13.3333 9.99984 13.3333Z"
-      stroke="black"
-      strokeWidth="1.75"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    />
-  </>
+  <path
+    d="M4.10817 4.10835L7.6415 7.64169M12.3582 7.64169L15.8915 4.10835M12.3582 12.3584L15.8915 15.8917M7.6415 12.3584L4.10817 15.8917M18.3332 10C18.3332 14.6024 14.6022 18.3334 9.99984 18.3334C5.39746 18.3334 1.6665 14.6024 1.6665 10C1.6665 5.39765 5.39746 1.66669 9.99984 1.66669C14.6022 1.66669 18.3332 5.39765 18.3332 10ZM13.3332 10C13.3332 11.841 11.8408 13.3334 9.99984 13.3334C8.15889 13.3334 6.6665 11.841 6.6665 10C6.6665 8.15907 8.15889 6.66669 9.99984 6.66669C11.8408 6.66669 13.3332 8.15907 13.3332 10Z"
+    stroke="black"
+    strokeWidth="1.75"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+  />
 );
