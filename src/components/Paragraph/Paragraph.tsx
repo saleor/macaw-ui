2d4f8436f44ba93ddad4952c900b8682d2028e54
Wojciech <wojciech.mista@hotmail.com>
2024-10-03 14:05:35 +0200

    change tag to p

--- src/components/Paragraph/Paragraph.tsx
+++ src/components/Paragraph/Paragraph.tsx
@@ -3,7 +3,7 @@ import { TextProps, Text } from "../Text";
 
 export const Paragraph = forwardRef<HTMLSpanElement, TextProps>(
   ({ ...rest }, ref) => {
-    return <Text ref={ref} display="block" {...rest} />;
+    return <Text ref={ref} display="block" as="p" {...rest} />;
   }
 );
 
