eeb04b9fa51c025cddd73a66324df8f1c3086fe3
Paweł Chyła <pawel.chyla@saleor.io>
2025-03-07 10:07:18 +0100

    Revert "Revert "Fix floating ui (#880)" (#895)"
    This reverts commit 84e01e3308d56547360010243c8f76a2646939e7.

--- src/components/Select/SelectWrapper.tsx
+++ src/components/Select/SelectWrapper.tsx
@@ -2,7 +2,7 @@
   Do not expose this file, it's for internal purposes only.
 */
 import { UseSelectPropGetters } from "downshift";
-import { ReactNode, forwardRef } from "react";
+import { ReactNode } from "react";
 
 import { classNames } from "~/utils";
 
@@ -23,69 +23,60 @@ type SelectWrapperProps = LabelVariants & {
   getLabelProps: UseSelectPropGetters<Option>["getLabelProps"];
 };
 
-export const SelectWrapper = forwardRef<HTMLLabelElement, SelectWrapperProps>(
-  (
-    {
-      id,
-      label,
-      className,
-      error,
-      children,
-      getToggleButtonProps,
-      getLabelProps,
-      typed,
-      active,
-      disabled,
-      size,
-    },
-    ref
-  ) => {
-    return (
-      <Box ref={ref}>
+export const SelectWrapper = ({
+  id,
+  label,
+  className,
+  error,
+  children,
+  getToggleButtonProps,
+  getLabelProps,
+  typed,
+  active,
+  disabled,
+  size,
+}: SelectWrapperProps) => {
+  return (
+    <Box
+      as="label"
+      className={classNames(
+        labelRecipe({ typed, active, disabled, size, error }),
+        className
+      )}
+      alignItems="center"
+      justifyContent="space-between"
+      disabled={disabled}
+      flexWrap="nowrap"
+      gap={3}
+      {...getToggleButtonProps()}
+      data-macaw-ui-component="Select"
+      cursor={disabled ? "not-allowed" : "pointer"}
+    >
+      <Box
+        display="flex"
+        flexDirection="column"
+        // Take full width minus the arrow icon width
+        __width="calc(100% - 30px)"
+      >
         <Box
-          as="label"
-          className={classNames(
-            labelRecipe({ typed, active, disabled, size, error }),
-            className
-          )}
-          alignItems="center"
-          justifyContent="space-between"
-          disabled={disabled}
-          flexWrap="nowrap"
-          gap={3}
-          {...getToggleButtonProps()}
-          data-macaw-ui-component="Select"
-          cursor={disabled ? "not-allowed" : "pointer"}
+          as="span"
+          className={classNames(spanRecipe({ typed, size, disabled, error }))}
+          {...getLabelProps({ htmlFor: id })}
         >
-          <Box
-            display="flex"
-            flexDirection="column"
-            // Take full width minus the arrow icon width
-            __width="calc(100% - 30px)"
-          >
-            <Box
-              as="span"
-              className={classNames(
-                spanRecipe({ typed, size, disabled, error })
-              )}
-              {...getLabelProps({ htmlFor: id })}
-            >
-              {label}
-            </Box>
-            {children}
-          </Box>
-
-          <ArrowDownIcon
-            className={classNames(
-              toggleIconStyle,
-              sprinkles({ cursor: "pointer" })
-            )}
-            size={size}
-          />
+          {label}
         </Box>
+        {children}
       </Box>
-    );
-  }
-);
+
+      <ArrowDownIcon
+        className={classNames(
+          toggleIconStyle,
+          sprinkles({ cursor: "pointer" })
+        )}
+        size={size}
+      />
+    </Box>
+  );
+};
 
 SelectWrapper.displayName = "SelectWrapper";
